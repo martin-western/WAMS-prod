569cd350fe26b9acb53d56f337927ef515ddb899
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2020-01-04 14:37:52 +0530

    small fixes

--- WAMS/settings.py
+++ WAMS/settings.py
@@ -64,6 +64,7 @@ WSGI_APPLICATION = 'WAMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -71,6 +72,7 @@ DATABASES = {
     }
 }
 """
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -81,7 +83,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
