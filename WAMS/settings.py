0670149db37f0992ad958aec9c30473b71ef31a0
raj1238 <shahrajyogesh@gmail.com>
2020-01-07 18:33:37 +0530

    Refactoring 32

--- WAMS/settings.py
+++ WAMS/settings.py
@@ -64,25 +64,26 @@ WSGI_APPLICATION = 'WAMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wamstest2',
+        'NAME': 'wamstest',
         'USER': 'nisarg',
         'PASSWORD': 'nisargtike',
         'HOST': 'localhost',
         'PORT': '3306',
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
