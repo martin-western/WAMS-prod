de99e081f41c2bfb4b2c98969b7aa7bdac3d672b
Raj Shah <shahrajyogesh@gmail.com>
2020-01-15 19:39:15 +0530

    product-details-page

--- WAMS/settings.py
+++ WAMS/settings.py
@@ -64,15 +64,15 @@ WSGI_APPLICATION = 'WAMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 """
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -83,7 +83,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
