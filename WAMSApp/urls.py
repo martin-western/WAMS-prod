77ef25b13a585e92785fc96b0b8385cc446de4e0
raj1238 <shahrajyogesh@gmail.com>
2020-01-16 14:35:51 +0530

    Git Machate Hai 4

--- WAMSApp/urls.py
+++ WAMSApp/urls.py
@@ -38,7 +38,6 @@ urlpatterns = [
 
 
     # Flyers and PFL
-<<<<<<< HEAD
     url(r'^create-flyer/$', views.CreateFlyer),
     url(r'^fetch-flyer-details/$', views.FetchFlyerDetails),
     url(r'^create-pfl/$', views.CreatePFL),
@@ -114,50 +113,7 @@ urlpatterns = [
 
     url(r'save-base-product/$', views.SaveBaseProduct),
     
-=======
-    url(r'^create-flyer/', views.CreateFlyer),
-    url(r'^fetch-flyer-details/', views.FetchFlyerDetails),
-    url(r'^create-pfl/', views.CreatePFL),
-    url(r'^fetch-pfl-details/', views.FetchPFLDetails),
-    url(r'^fetch-product-list-flyer-pfl/', views.FetchProductListFlyerPFL),
-    url(r'^add-product-flyer-bucket/', views.AddProductFlyerBucket),
-    url(r'^add-product-pfl-bucket/', views.AddProductPFLBucket),
-    url(r'^fetch-product-details-flyer-pfl/', views.FetchProductDetailsFlyerPFL),   
-    url(r'^save-flyer-template/', views.SaveFlyerTemplate),
-    url(r'^save-pfl-template/', views.SavePFLTemplate),
-    url(r'^upload-image-external-bucket-flyer/', views.UploadImageExternalBucketFlyer),
-    url(r'^upload-image-external-bucket-pfl/', views.UploadImageExternalBucketPFL),
-
-
-    url(r'^pfl/(?P<pk>[\w-]+)/', views.PFLPage),
-    url(r'^pfl-dashboard/', views.PFLDashboardPage),
-    url(r'^fetch-pfl-list/', views.FetchPFLList),
-
-
-    url(r'^flyer/(?P<pk>[\w-]+)/', views.FlyerPage),
-    url(r'^flyer-dashboard/', views.FlyerDashboardPage),
-    url(r'^fetch-flyer-list/', views.FetchFlyerList),
-
-    url(r'^upload-new-flyer-bg-image/', views.UploadNewFlyerBGImage),
-    url(r'^download-images-s3/', views.DownloadImagesS3),
-    url(r'^fetch-brands/', views.FetchBrands),      
-
-    url(r'^save-pfl-in-bucket/', views.SavePFLInBucket),            
-    url(r'^save-flyer-in-bucket/', views.SaveFlyerInBucket),            
-
-    url(r'^verify-product/', views.VerifyProduct),
-
-    url(r'^delete-image/', views.DeleteImage),
-
-    url(r'^remove-product-from-export-list/', views.RemoveProductFromExportList),
-
-    url(r'^download-product/', views.DownloadProduct),
-    url(r'^ecommerce-listing/(?P<pk>[\w-]+)/', views.EcommerceListingPage),
-
-    url(r'^upload-flyer-external-images/', views.UploadFlyerExternalImages),
-    url(r'^upload-pfl-external-images/', views.UploadPFLExternalImages),
 
     url(r'^sap-integration',views.SapIntegration),
 
->>>>>>> b04dce2a517e50f8e97f17afb36c686bac8bb682
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
