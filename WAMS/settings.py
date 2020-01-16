c113e7f21ed077996d0bdb30cff86afcba436eaf
raj1238 <shahrajyogesh@gmail.com>
2020-01-16 14:42:45 +0530

    Git Machate Hai 8

--- WAMS/settings.py
+++ WAMS/settings.py
@@ -64,25 +64,25 @@ WSGI_APPLICATION = 'WAMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wams_prod2',
-        'USER': 'nisarg',
-        'PASSWORD': 'nisargtike',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'wams_prod2',
+#         'USER': 'nisarg',
+#         'PASSWORD': 'nisargtike',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
