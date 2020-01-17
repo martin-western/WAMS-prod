eae65b05de660f5c85ae2f9f9faa1c1f5738ad8e
Raj Shah <shahrajyogesh@gmail.com>
2020-01-17 18:28:26 +0530

    Migrate TO Channel Split 2

--- scripts/migrate_to_channel_split.py
+++ scripts/migrate_to_channel_split.py
@@ -22,50 +22,50 @@ organization_pk_mapping = {}
 category_pk_mapping = {}
 
 
-cnt=0
+image_cnt=0
 for data in all_data_json:
     
     try:
         if data["model"] == "WAMSApp.image":
-            cnt+=1
+            image_cnt+=1
             image_obj = Image.objects.create(image=data["fields"]["image"])
             image_pk_mapping[data["pk"]] = image_obj.pk
-            print("Cnt:", cnt)
+            print("Image Cnt:", image_cnt)
     except Exception as e:
         print("Error Image", str(e))
 
-cnt=0
+brand_cnt=0
 for data in all_data_json:
     
     try:
         if data["model"] == "WAMSApp.brand":
-            cnt+=1
+            brand_cnt+=1
             brand_obj = Brand.objects.create(name=data["fields"]["name"])
             brand_pk_mapping[data["pk"]] = brand_obj.pk
-            print("Cnt:", cnt)
+            print("Cnt:", brand_cnt)
     except Exception as e:
         print("Error Brnad", str(e))
 
-cnt=0
+organization_cnt=0
 for data in all_data_json:
     
     try:
         if data["model"] == "WAMSApp.organization":
-            cnt+=1
+            organization_cnt+=1
             organization_obj = Organization.objects.create(name=data["fields"]["name"])
             organization_pk_mapping[data["pk"]] = organization_obj.pk
-            print("Cnt:", cnt)
+            print("Cnt:", organization_cnt)
     except Exception as e:
         print("Error Organization", str(e))
 
-cnt=0
+category_cnt=0
 for data in all_data_json:
     
     try:
         if data["model"] == "WAMSApp.category":
-            cnt+=1
+            category_cnt+=1
             category_obj = Category.objects.create(name=data["fields"]["name"])
             category_pk_mapping[data["pk"]] = category_obj.pk
-            print("Cnt:", cnt)
+            print("Cnt:", category_cnt)
     except Exception as e:
         print("Error Category", str(e))
