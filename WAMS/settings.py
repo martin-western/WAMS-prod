7a7ad800f72b2686144aee30fb53df7c08b05f1d
Raj Shah <shahrajyogesh@gmail.com>
2020-01-20 12:25:48 +0530

    added dimenssions in amazon uk form

--- WAMS/settings.py
+++ WAMS/settings.py
@@ -65,24 +65,24 @@ WSGI_APPLICATION = 'WAMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wamstest3',
-        'USER': 'nisarg',
-        'PASSWORD': 'nisargtike',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'wamstest3',
+#         'USER': 'nisarg',
+#         'PASSWORD': 'nisargtike',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
