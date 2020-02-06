be07ba348285b01117fbab80a6c5f4ebd4698379
Raj Shah <shahrajyogesh@gmail.com>
2020-02-06 18:07:51 +0530

    prices Cache

--- WAMSApp/utils.py
+++ WAMSApp/utils.py
@@ -91,10 +91,10 @@ def fetch_prices(product_id):
 
         # Check if Cached
         product_obj = Product.objects.filter(base_product__seller_sku=product_id)[0]
-        # curr_time = timezone.now()
-        # if (product_obj.sap_cache_time-curr_time).seconds<86400:
-        #     warehouse_information = json.loads(product_obj.sap_cache)
-        #     return warehouse_information
+        curr_time = timezone.now()
+        if (product_obj.sap_cache_time-curr_time).seconds<86400:
+             warehouse_information = json.loads(product_obj.sap_cache)
+             return warehouse_information
 
         url="http://94.56.89.114:8001/sap/bc/srt/rfc/sap/zser_stock_price/300/zser_stock_price/zbin_stock_price"
         headers = {'content-type':'text/xml','accept':'application/json','cache-control':'no-cache'}
@@ -208,9 +208,9 @@ def fetch_prices(product_id):
             
             warehouse_information.append(warehouse_dict)
 
-        # product_obj.sap_cache = json.dumps(warehouse_information)
-        # product_obj.sap_cache_time = curr_time
-        # product_obj.save()
+        product_obj.sap_cache = json.dumps(warehouse_information)
+        product_obj.sap_cache_time = curr_time
+        product_obj.save()
         
         return warehouse_information
 
