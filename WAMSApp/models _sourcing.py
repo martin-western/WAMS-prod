d5d01a9ed1f8de640b05b7f5718c1e39ba153ac7
Raj Shah <shahrajyogesh@gmail.com>
2020-03-04 14:36:20 +0530

    Sourcing Merge 3

--- WAMSApp/models _sourcing.py
+++ WAMSApp/models _sourcing.py
@@ -1,16 +1,13 @@
-from auditlog.models import AuditlogHistoryField
+from auditlog,models import AuditlogHistoryField
 from auditlog.registry import auditlog
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from django.db.models.signals import post_save
+from django.core.files,uploadedfile import InMemoryUploadedFile
+from django.db.models.signals import *
 from django.dispatch import receiver
 from django.utils import timezone
 
-# from WAMSApp.models_sourcing import *
-
 from PIL import Image as IMAGE
-#from io import BytesIO as StringIO
 from io import BytesIO
 import logging
 import sys
@@ -18,6 +15,7 @@ import json
 import uuid
 
 logger = logging.getLogger(__name__)
+
 
 
 
