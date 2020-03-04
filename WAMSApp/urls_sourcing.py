d6df66f66e854a9ac994d0dd5ed898cb8436529d
Raj Shah <shahrajyogesh@gmail.com>
2020-03-04 16:50:55 +0530

    Sourcing Merge 16

--- WAMSApp/urls_sourcing.py
+++ WAMSApp/urls_sourcing.py
@@ -6,135 +6,132 @@ from django.http import HttpResponseRedirect
 
 urlpatterns = [
 
-    url(r'^current_user/', views.current_user),
-    url(r'^users/', views.UserList.as_view()),
+    url(r'^$', views.RedirectHome),
+    url(r'^login/$', views.Login),
+    url(r'^login-submit/$', views.LoginSubmit),
 
-    url(r'^fetch-constant-values/$', views.FetchConstantValues),
-    url(r'^create-new-base-product/$', views.CreateNewBaseProduct),
-    url(r'^create-new-product/$', views.CreateNewProduct),
+    url(r'^logout/$', views.Logout),
+    url(r'^home/$', views.Home),
+    url(r'^factory-products/$', views.FactoryProducts),
+    url(r'^sourcing-products/$', views.SourcingProducts),
 
-    url(r'^edit-product/(?P<pk>[\w-]+)/$', views.EditProductPage),
+    url(r'^fetch-factories/$', views.FetchFactories),
+    url(r'^add-factory/$', views.AddFactoryPage),
+    url(r'^add-new-factory/$', views.AddNewFactory),
+    url(r'^factory/(?P<pk>[\w-]+)/$', views.FactoryPage),
+    url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
+    url(r'^new-product/(?P<pk>[\w-]+)/$', views.NewProductPage),
+    url(r'^fetch-constants/$', views.FetchConstants),
+    url(r'^add-new-product/$', views.AddNewProduct),
+
+    url(r'^upload-factories-and-products/$', views.UploadFactoriesProducts),
+
+
+    url(r'^product/(?P<pk>[\w-]+)/$', views.ProductPage),
     url(r'^fetch-product-details/$', views.FetchProductDetails),
-    url(r'^fetch-base-product-details/$', views.FetchBaseProductDetailsAPI),
-    url(r'^save-product/$', views.SaveProduct),
-
-    # url(r'^save-pfl-image/', views.SavePflImage),
-
-    url(r'^products/$', views.Products),
-    url(r'^fetch-product-list/$', views.FetchProductList),
-    url(r'^fetch-export-list/$', views.FetchExportList),
-    url(r'^add-to-export/$', views.AddToExport),
-
-    url(r'^export-list/$', views.ExportListPage),
-    url(r'^fetch-export-product-list/$', views.FetchExportProductList),
-    url(r'^download-export-list/$', views.DownloadExportList),
-
-    url(r'^import-products/$', views.ImportProducts),
-
+    url(r'^save-phone-numbers/$', views.SavePhoneNumbers),
+    url(r'^save-address/$', views.SaveAddress),
+    url(r'^save-factory-name/$', views.SaveFactoryName),
+    url(r'^save-product-details/$', views.SaveProductDetails),
+    url(r'^save-business-card/$', views.SaveBusinessCard),
     url(r'^upload-product-image/$', views.UploadProductImage),
-
-    url(r'^update-main-image/$', views.UpdateMainImage),
-    url(r'^update-sub-images/$', views.UpdateSubImages),
-
-
-    # Flyers and PFL
-    url(r'^create-flyer/$', views.CreateFlyer),
-    url(r'^fetch-flyer-details/$', views.FetchFlyerDetails),
-    url(r'^create-pfl/$', views.CreatePFL),
-    url(r'^fetch-pfl-details/$', views.FetchPFLDetails),
-    url(r'^fetch-product-list-flyer-pfl/$', views.FetchProductListFlyerPFL),
-    url(r'^add-product-flyer-bucket/$', views.AddProductFlyerBucket),
-    url(r'^add-product-pfl-bucket/$', views.AddProductPFLBucket),
-    url(r'^fetch-product-details-flyer-pfl/$',
-        views.FetchProductDetailsFlyerPFL),
-    url(r'^save-flyer-template/$', views.SaveFlyerTemplate),
-    url(r'^save-pfl-template/$', views.SavePFLTemplate),
-    url(r'^upload-image-external-bucket-flyer/$',
-        views.UploadImageExternalBucketFlyer),
-    url(r'^upload-image-external-bucket-pfl/$',
-        views.UploadImageExternalBucketPFL),
-
-
-    url(r'^pfl/(?P<pk>[\w-]+)/$', views.PFLPage),
-    url(r'^pfl-dashboard/$', views.PFLDashboardPage),
-    url(r'^fetch-pfl-list/$', views.FetchPFLList),
-
-
-    url(r'^flyer/(?P<pk>[\w-]+)/$', views.FlyerPage),
-    url(r'^flyer-dashboard/$', views.FlyerDashboardPage),
-    url(r'^fetch-flyer-list/$', views.FetchFlyerList),
-
-    url(r'^upload-new-flyer-bg-image/$', views.UploadNewFlyerBGImage),
-    url(r'^download-images-s3/$', views.DownloadImagesS3),
-    url(r'^fetch-brands/$', views.FetchBrands),
-
-    url(r'^save-pfl-in-bucket/$', views.SavePFLInBucket),
-    url(r'^save-flyer-in-bucket/$', views.SaveFlyerInBucket),
-
-    url(r'^verify-product/$', views.VerifyProduct),
-
     url(r'^delete-image/$', views.DeleteImage),
+    url(r'^upload-attachment/$', views.UploadAttachment),
+    url(r'^delete-attachment/$', views.DeleteAttachment),
 
-    url(r'^remove-product-from-export-list/$',
-        views.RemoveProductFromExportList),
+    url(r'^fetch-product-cards/$', views.FetchProductCards),
+    url(r'^save-factory-details/$', views.SaveFactoryDetails),
 
-    url(r'^download-product/$', views.DownloadProduct),
-    url(r'^ecommerce-listing/(?P<pk>[\w-]+)/$', views.EcommerceListingPage),
+    url(r'^export-factories/$', views.ExportFactories),
 
-    url(r'^upload-flyer-external-images/$', views.UploadFlyerExternalImages),
-    url(r'^upload-pfl-external-images/$', views.UploadPFLExternalImages),
+    url(r'^search-factories/$', views.SearchFactories),
+    url(r'^search-products/$', views.SearchProducts),
+    url(r'^share-factory/$', views.ShareFactory),
+    url(r'^send-factory-share-email/$', views.SendFactoryShareEmail),
+    url(r'^signup/$', views.SignUpFactoryManager),
+    url(r'^signup-submit/$', views.SignUpSubmit),
+    url(r'^fetch-factory-name-from-uuid/$', views.FetchFactoryNameFromUUID),
 
-    url(r'^channel-product/amazon-uk/(?P<pk>[\w-]+)/$',
-        views.ChannelProductAmazonUKPage),
-    url(r'^channel-product/amazon-uae/(?P<pk>[\w-]+)/$',
-        views.ChannelProductAmazonUAEPage),
-    url(r'^channel-product/ebay/(?P<pk>[\w-]+)/$',
-        views.ChannelProductEbayPage),
-    url(r'^channel-product/noon/(?P<pk>[\w-]+)/$',
-        views.ChannelProductNoonPage),
+    url(r'^login-factory-manager/$', views.LoginFactoryManager),
+    url(r'^login-submit-factory-manager/$', views.LoginSubmitFactoryManager),
+    url(r'^logout-factory-manager/$', views.LogoutFactoryManager),
+    url(r'^fetch-factory-for-sourcing-user/$',
+        views.FetchFactoryForSourcingUser),
 
-    url(r'^save-channel-product-amazon-uk/$',
-        views.SaveAmazonUKChannelProduct),
-    url(r'^save-channel-product-amazon-uae/$',
-        views.SaveAmazonUAEChannelProduct),
-    url(r'^save-channel-product-ebay/$',
-        views.SaveEbayChannelProduct),
-    url(r'^save-channel-product-noon/$',
-        views.SaveNoonChannelProduct),
 
-    url(r'^fetch-channel-product-amazon-uk/$',
-        views.FetchAmazonUKChannelProduct),
-    url(r'^fetch-channel-product-amazon-uae/$',
-        views.FetchAmazonUAEChannelProduct),
-    url(r'^fetch-channel-product-ebay/$',
-        views.FetchEbayChannelProduct),
-    url(r'^fetch-channel-product-noon/$',
-        views.FetchNoonChannelProduct),
+    url(r'^factories-for-sourcing-user/$', views.FactoriesForSourcingUser),
+    url(r'^fetch-factories-for-sourcing-user/$',
+        views.FetchFactoriesForSourcingUser),
 
-    url(r'save-base-product/$', views.SaveBaseProduct),
-    
-    url(r'^sap-integration/$',views.SapIntegration),
-    url(r'^fetch-user-profile/$',views.FetchUserProfile),
+    url(r'^fetch-sourcing-user-details/$', views.FetchSourcingUserDetails),
 
-    url(r'^fetch-dealshub-products/$',views.FetchDealsHubProducts),
+    url(r'^fetch-products-from-factory/$', views.FetchProductsFromFactory),
+    url(r'^save-factory-product-details/$', views.SaveFactoryProductDetails),
+    url(r'^share-product/$', views.ShareProduct),
 
-    url(r'^fetch-audit-logs-by-user/$',views.FetchAuditLogsByUser), 
-    url(r'^fetch-audit-logs/$',views.FetchAuditLogs), 
-    url(r'^create-request-help/$',views.CreateRequestHelp),    
+    url(r'^fetch-shared-products-for-factory/$',
+        views.FetchSharedProductsForFactory),
+    url(r'^factory-product/(?P<pk>[\w-]+)/$', views.FactoryProduct),
+    url(r'^sourcing-product/(?P<pk>[\w-]+)/$', views.SourcingProduct),
+    url(r'^factory-profile-for-sourcing/(?P<pk>[\w-]+)/$',
+        views.FactoryProfileForSourcing),
+    url(r'^proforma-invoice/(?P<pk>[\w-]+)/$', views.ProformaInvoiceHome),
+    # fetch-draft-proforma-invoice
+    url(r'^fetch-draft-proforma-invoice/$', views.FetchDraftProformaInvoice),
+    url(r'^download-pi-bulk/$', views.DownloadPIBulk),
+    # download-pi-bulk` create-draft-pi-from-product-selection
+    url(r'^create-draft-pi-from-product-selection/$',
+        views.CreateDraftPIFromProductSelection),
 
-    url(r'^fetch-channel-product-list/$',views.FetchChannelProductList),
 
-    url(r'^save-company-profile/$',views.SaveCompanyProfile),
-    url(r'^upload-organization-logo/$',views.UploadOrganizationLogo),
-    url(r'^fetch-company-profile/$',views.FetchCompanyProfile),
 
-    url(r'^refresh-page-price-and-stock/$',views.RefreshPagePriceAndStock),
-    url(r'^refresh-product-price-and-stock/$',views.RefreshProductPriceAndStock),
-    url(r'^fetch-product-details-sales-integration/$',views.FetchProductDetailsSalesIntegration),
 
-    url(r'^move-to-main-images/$',views.MoveToMainImages),
-    url(r'^move-to-sub-images/$',views.MoveToSubImages),
-    
+    url(r'^sourcing-factorywise-products/(?P<pk>[\w-]+)/$',
+        views.FetchProductsFacorywise),
+    url(r'^save-sourcing-product-details/$',
+        views.SaveSourcingProductDetails),
+
+    url(r'^save-sourcing-factory-details/$', views.SaveSourcingFactoryDetails),
+
+    url(r'^fetch-factorywise-product-listing/$',
+        views.FetchFactorywiseProductListing),
+
+
+
+    url(r'^fetch-factory-product-details/$', views.FetchFactoryProductDetails),
+    url(r'^fetch-sourcing-product-details/$',
+        views.FetchSourcingProductDetails),
+
+    url(r'^factory-shared-products/$', views.FactorySharedProducts),
+    url(r'^upload-factory-product-image/$', views.UploadFactoryProductImage),
+
+    url(r'^upload-factory-image/$', views.UploadFactoryImage),
+
+
+    url(r'^search-factories/$', views.SearchFactories),
+    url(r'^search-factories-by-date/$', views.SearchFactoriesByDate),
+    url(r'^search-products/$', views.SearchProducts),
+
+    url(r'^change-go-live-status/$', views.ChangeGoLiveStatus),
+    url(r'^download-pi/$', views.DownloadPI),
+    url(r'^generate-draft-pi-line/$', views.GenerateDraftPILine),
+    # generate-draft-pi-line   generate-pi-in-bulk
+    url(r'^upload-factories-and-products-sourcing-users/$',
+        views.UploadFactoriesProductsFromSourcing),
+    url(r'^upload-sourcing-user-product-image/$',
+        views.UploadSourcingProductProductImage),
+
+    url(r'^delete-draft-line/$',
+        views.DeleteDraftLine),
+    url(r'^proforma-invoices/$', views.FetchProformaInvoices),
+    url(r'^proforma-invoices-list/$',
+        views.FetchProformaInvoiceList),
+
+    url(r'^draft-proforma-invoices/$', views.DraftProformaInvoices),
+    url(r'^fetch-draft-proforma-invoice-cart/$',
+        views.FetchDraftProformaInvoicesCart),
+
+    #
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
