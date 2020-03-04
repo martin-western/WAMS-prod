623f228896ac8985c036f80607a35ede76351552
Raj Shah <shahrajyogesh@gmail.com>
2020-03-04 15:49:53 +0530

    Sourcing Merge 5

--- WAMSApp/models _sourcing.py
+++ WAMSApp/models _sourcing.py
@@ -7,6 +7,7 @@ from django.db.models.signals import *
 from django.dispatch import receiver
 from django.utils import timezone
 
+from WAMSApp.models import *
 from PIL import Image as IMAGE
 from io import BytesIO
 import logging
@@ -38,6 +39,50 @@ class SourcingUserProduct(object):
     class Meta:
         verbose_name = "SourcingUserProduct"
         verbose_name_plural = "SourcingUserProducts"
+
+class Bank(models.Model): ### As it is in WAMS
+
+    name = models.CharField(max_length=300, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    account_number = models.CharField(max_length=300, null=True, blank=True)
+    ifsc_code = models.CharField(max_length=300, null=True, blank=True)
+    swift_code = models.CharField(max_length=300, null=True, blank=True)
+    branch_code = models.CharField(max_length=300, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Bank"
+        verbose_name_plural = "Banks"
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        super(Bank, self).save(*args, **kwargs)
+
+
+class Factory(models.Model):  ### As it is in WAMS
+
+    business_card = models.ForeignKey(Image, null=True, blank=True, on_delete=models.SET_NULL, related_name="business_card")
+    phone_numbers = models.ManyToManyField(PhoneNumber, blank=True)
+    factory_emailid = models.CharField(max_length=300, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    operating_hours = models.ManyToManyField(OperatingHour, blank=True)
+    bank_details = models.ForeignKey(Bank, null=True, blank=True, on_delete=models.SET_NULL, related_name="related_factory")
+    average_delivery_days = models.IntegerField(null=True, blank=True)
+    average_turn_around_time = models.IntegerField(null=True, blank=True)
+    logo = models.ForeignKey(Image, null=True, blank=True, on_delete=models.SET_NULL)
+    contact_person_name = models.CharField(max_length=300, null=True, blank=True)
+    contact_person_emailid = models.CharField(max_length=300, null=True, blank=True)
+    contact_person_mobile_no = models.CharField(max_length=300, null=True, blank=True)
+    social_media_tag = models.CharField(max_length=300, null=True, blank=True)
+    social_media_tag_information = models.CharField(max_length=300, null=True, blank=True)
+    loading_port = models.CharField(max_length=300, null=True, blank=True)
+    location = models.CharField(max_length=300, null=True, blank=True)
+    created_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "BaseFactory"
+        verbose_name_plural = "BaseFactories"
 
 
 class ProformaInvoice(models.Model): #### As it is in WAMS
