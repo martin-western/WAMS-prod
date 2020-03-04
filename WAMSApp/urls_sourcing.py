e811996afaf32ca49b957c24b498b71be93a8b9c
Raj Shah <shahrajyogesh@gmail.com>
2020-03-04 16:54:07 +0530

    Sourcing Merge 17

--- WAMSApp/urls_sourcing.py
+++ WAMSApp/urls_sourcing.py
@@ -6,28 +6,15 @@ from django.http import HttpResponseRedirect
 
 urlpatterns = [
 
-    url(r'^$', views.RedirectHome),
-    url(r'^login/$', views.Login),
-    url(r'^login-submit/$', views.LoginSubmit),
-
-    url(r'^logout/$', views.Logout),
-    url(r'^home/$', views.Home),
-    url(r'^factory-products/$', views.FactoryProducts),
-    url(r'^sourcing-products/$', views.SourcingProducts),
-
     url(r'^fetch-factories/$', views.FetchFactories),
-    url(r'^add-factory/$', views.AddFactoryPage),
     url(r'^add-new-factory/$', views.AddNewFactory),
-    url(r'^factory/(?P<pk>[\w-]+)/$', views.FactoryPage),
     url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
-    url(r'^new-product/(?P<pk>[\w-]+)/$', views.NewProductPage),
     url(r'^fetch-constants/$', views.FetchConstants),
     url(r'^add-new-product/$', views.AddNewProduct),
 
     url(r'^upload-factories-and-products/$', views.UploadFactoriesProducts),
 
 
-    url(r'^product/(?P<pk>[\w-]+)/$', views.ProductPage),
     url(r'^fetch-product-details/$', views.FetchProductDetails),
     url(r'^save-phone-numbers/$', views.SavePhoneNumbers),
     url(r'^save-address/$', views.SaveAddress),
@@ -48,18 +35,12 @@ urlpatterns = [
     url(r'^search-products/$', views.SearchProducts),
     url(r'^share-factory/$', views.ShareFactory),
     url(r'^send-factory-share-email/$', views.SendFactoryShareEmail),
-    url(r'^signup/$', views.SignUpFactoryManager),
-    url(r'^signup-submit/$', views.SignUpSubmit),
     url(r'^fetch-factory-name-from-uuid/$', views.FetchFactoryNameFromUUID),
 
-    url(r'^login-factory-manager/$', views.LoginFactoryManager),
-    url(r'^login-submit-factory-manager/$', views.LoginSubmitFactoryManager),
-    url(r'^logout-factory-manager/$', views.LogoutFactoryManager),
     url(r'^fetch-factory-for-sourcing-user/$',
         views.FetchFactoryForSourcingUser),
 
 
-    url(r'^factories-for-sourcing-user/$', views.FactoriesForSourcingUser),
     url(r'^fetch-factories-for-sourcing-user/$',
         views.FetchFactoriesForSourcingUser),
 
@@ -71,12 +52,6 @@ urlpatterns = [
 
     url(r'^fetch-shared-products-for-factory/$',
         views.FetchSharedProductsForFactory),
-    url(r'^factory-product/(?P<pk>[\w-]+)/$', views.FactoryProduct),
-    url(r'^sourcing-product/(?P<pk>[\w-]+)/$', views.SourcingProduct),
-    url(r'^factory-profile-for-sourcing/(?P<pk>[\w-]+)/$',
-        views.FactoryProfileForSourcing),
-    url(r'^proforma-invoice/(?P<pk>[\w-]+)/$', views.ProformaInvoiceHome),
-    # fetch-draft-proforma-invoice
     url(r'^fetch-draft-proforma-invoice/$', views.FetchDraftProformaInvoice),
     url(r'^download-pi-bulk/$', views.DownloadPIBulk),
     # download-pi-bulk` create-draft-pi-from-product-selection
@@ -86,8 +61,6 @@ urlpatterns = [
 
 
 
-    url(r'^sourcing-factorywise-products/(?P<pk>[\w-]+)/$',
-        views.FetchProductsFacorywise),
     url(r'^save-sourcing-product-details/$',
         views.SaveSourcingProductDetails),
 
@@ -102,7 +75,6 @@ urlpatterns = [
     url(r'^fetch-sourcing-product-details/$',
         views.FetchSourcingProductDetails),
 
-    url(r'^factory-shared-products/$', views.FactorySharedProducts),
     url(r'^upload-factory-product-image/$', views.UploadFactoryProductImage),
 
     url(r'^upload-factory-image/$', views.UploadFactoryImage),
@@ -123,11 +95,9 @@ urlpatterns = [
 
     url(r'^delete-draft-line/$',
         views.DeleteDraftLine),
-    url(r'^proforma-invoices/$', views.FetchProformaInvoices),
     url(r'^proforma-invoices-list/$',
         views.FetchProformaInvoiceList),
 
-    url(r'^draft-proforma-invoices/$', views.DraftProformaInvoices),
     url(r'^fetch-draft-proforma-invoice-cart/$',
         views.FetchDraftProformaInvoicesCart),
 
