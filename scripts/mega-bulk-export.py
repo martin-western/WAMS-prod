3d030f99fdb86c6d6e53eae645e7c3a989f36d8a
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2020-03-05 15:51:51 +0530

    in progress mega bulk export

--- scripts/mega-bulk-export.py
+++ scripts/mega-bulk-export.py
@@ -13,22 +13,60 @@ cell_format = workbook.add_format({'bold': True})
 cell_format.set_pattern(1)
 cell_format.set_bg_color('yellow')
 
-rownum = 0
-colnum = 0
-with open('./WAMSApp/static/WAMSApp/csv/amazon_uk.csv','rt')as f:
-    data = csv.reader(f)
-    for row in data:
-        colnum = 0
-        for rowdata in row:
-            worksheet.write(rownum, colnum, rowdata, cell_format)
-            colnum += 1
-        rownum += 1
+products = Product.objects.filter(base_product__organization__name="geepas")
 
+cnt = 0
 for product in products:
     try:
-        base_product = product.base_product
-        channel_product = product.channel_product
-        amazon_uk_product = json.loads(channel_product.amazon_uk_product_json)
-
-        common_row = ["" for i in range(213)]
-        common_row[0] = base_product.seller_sku 
+        common_row = ["" for i in range(176)]
+        cnt += 1
+        common_row[0] = cnt
+        common_row[1] = product.product_id
+        common_row[2] = product.product_name
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.category)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
+        common_row[3] = str(product.base_product.brand)
