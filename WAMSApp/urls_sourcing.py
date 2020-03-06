755fd48c94ab858d3f0207317108bd088065e00e
Raj Shah <shahrajyogesh@gmail.com>
2020-03-06 11:59:23 +0530

    Sourcing Merge 36

--- WAMSApp/urls_sourcing.py
+++ WAMSApp/urls_sourcing.py
@@ -6,35 +6,27 @@ from django.http import HttpResponseRedirect
 
 urlpatterns = [
 
-    
-FetchFactoryDetails = FetchFactoryDetailsAPI.as_view() 
+    url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
+    url(r'^fetch-factories/$', views.FetchFactoriesList),
+    url(r'^fetch-products-from-factory/$', views.FetchProductsFromFactory),
+    url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
+    url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
+    url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
+    url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
+    url(r'^fetch-factory-details/$', views.FetchFactoryDetails),
 
-FetchFactoriesList = FetchFactoriesListAPI.as_view()
-
-FetchProductsFromFactory = FetchProductsFromFactoryAPI.as_view()
 
 SaveFactoryDetails = SaveFactoryDetailsAPI.as_view()
-
 FetchFactorywiseProductListing = FetchFactorywiseProductListingAPI.as_view()
-
 UploadFactoryImage = UploadFactoryImageAPI.as_view()
-
 ChangeGoLiveStatus = ChangeGoLiveStatusAPI.as_view()
-
 DownloadPI = DownloadPIAPI.as_view()
-
 DownloadPIBulk = DownloadPIBulkAPI.as_view()
-
 GenerateDraftPILine = GenerateDraftPILineAPI.as_view()
-
 FetchDraftProformaInvoice = FetchDraftProformaInvoiceAPI.as_view()
-
 DeleteDraftLine = DeleteDraftLineAPI.as_view()
-
 CreateDraftPIFromProductSelection = CreateDraftPIFromProductSelectionAPI.as_view()
-
 FetchProformaInvoiceList = FetchProformaInvoiceListAPI.as_view()
-
 FetchDraftProformaInvoicesCart = FetchDraftProformaInvoicesCartAPI.as_view()
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
