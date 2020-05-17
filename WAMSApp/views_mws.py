5acacd38a2ab335c0f50004bc369a6f5a622803e
Raj Shah <shahrajyogesh@gmail.com>
2020-05-17 16:16:34 +0530

    views_mws refactor 1

--- WAMSApp/views_mws.py
+++ WAMSApp/views_mws.py
@@ -1,48 +1,33 @@
-from django.http import HttpResponseRedirect
-from django.views.decorators.csrf import csrf_exempt
-
 from WAMSApp.models import *
 from WAMSApp.utils import *
 from WAMSApp.xml_generators import *
-from WAMSApp.utils_sourcing import *
 
 from MWS import mws,APIs
-
-from django.shortcuts import render, HttpResponse, get_object_or_404
-from django.contrib.auth import logout, authenticate, login
-from django.contrib.auth.decorators import login_required
 
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.core.paginator import Paginator
-from django.db.models import Q
 from django.db.models import Count
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django.utils import timezone
 
 import requests
 import json
-import os
 import pytz
 import csv
-import uuid
 import logging
 import sys
 import xlrd
-import zipfile
 import time
 
 
 from datetime import datetime
 from django.utils import timezone
 from django.core.files import File
-
 
 logger = logging.getLogger(__name__)
 
