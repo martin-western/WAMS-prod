a6aced33dac91c9b02b2e4b270cfbff0e699da92
Raj Shah <shahrajyogesh@gmail.com>
2020-06-08 19:25:39 +0530

    MWS Inventory Push 26

--- WAMSApp/xml_generators_uae.py
+++ WAMSApp/xml_generators_uae.py
@@ -18,9 +18,18 @@ def generate_xml_for_post_product_data_amazon_uae(product_pk_list,seller_id):
             product_obj = Product.objects.get(pk=int(product_pk))
             message_id = str(product_pk)
             seller_sku = product_obj.base_product.seller_sku
-            brand_name = product_obj.base_product.brand.name
-            product_id_type = product_obj.product_id_type.name
-            product_id = product_obj.barcode_string
+            
+            try:
+                brand_name = product_obj.base_product.brand.name
+            else:
+                brand_name = "None"
+
+            try:
+                product_id_type = product_obj.product_id_type.name
+            else:
+                product_id_type = "None"
+                
+            product_id = product_obj.product_id
             amazon_uae_product = json.loads(product_obj.channel_product.amazon_uae_product_json)
             product_name = amazon_uae_product["product_name"]
             product_description = amazon_uae_product["product_description"]
