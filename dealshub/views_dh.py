63ac358c981605795733c8cf603366b1e2968eee
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2020-07-12 18:44:17 +0530

    fix 6

--- dealshub/views_dh.py
+++ dealshub/views_dh.py
@@ -549,8 +549,7 @@ class PlaceOrderAPI(APIView):
                 unit_order_obj = UnitOrder.objects.create(order=order_obj, 
                                                           product=unit_cart_obj.product,
                                                           quantity=unit_cart_obj.quantity,
-                                                          price=unit_cart_obj.product.get_actual_price(), 
-                                                          currency=unit_cart_obj.product.get_currency())
+                                                          price=unit_cart_obj.product.get_actual_price(),
                 UnitOrderStatus.objects.create(unit_order=unit_order_obj)
 
             # Cart gets empty
