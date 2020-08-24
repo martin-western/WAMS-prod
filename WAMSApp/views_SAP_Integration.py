a26f28dc6327f54f8615c11989f35a6d459f1f59
raj1238 <shahrajyogesh@gmail.com>
2020-08-24 17:58:35 +0530

    SAP Automation 18

---
+++ WAMSApp/views_SAP_Integration.py
@@ -0,0 +1,34 @@
+from WAMSApp.models import *
+from WAMSApp.utils import *
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.core.paginator import Paginator
+from django.db.models import Count
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.utils import timezone
+
+import requests
+import json
+import pytz
+import csv
+import logging
+import sys
+import xlrd
+import time
+
+
+from datetime import datetime
+from django.utils import timezone
+from django.core.files import File
+
+logger = logging.getLogger(__name__)
+
+partner_id = "11109"
+country_code = "ae"
+partner_warehouse_code = "12345"
