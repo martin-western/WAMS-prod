a2d5ad2e30d46d8b9c578312b5e3fab8945dfb88
raj1238 <shahrajyogesh@gmail.com>
2020-08-24 16:40:39 +0530

    SAP Automation 15

--- WAMSApp/SAP_Automation_Test.py
+++ WAMSApp/SAP_Automation_Test.py
@@ -83,136 +83,185 @@ def fetch_prices(product_id,company_code,url,customer_id):
 
 
 response = fetch_prices(product_id,company_code,test_url,customer_id)
-print(response)
         
-# items = response["soap-env:Envelope"]["soap-env:Body"]["n0:ZAPP_STOCK_PRICE1Response"]["T_DATA"]["item"]
+items = response["soap-env:Envelope"]["soap-env:Body"]["n0:ZAPP_STOCK_PRICEResponse"]["T_DATA"]["item"]
 
-# if isinstance(items, dict):
-#     temp_qty = items["ATP_QTY"]
-#     if temp_qty!=None:
-#         temp_qty = float(temp_qty)
-#         qty = max(temp_qty, qty)
-#     temp_qty = items["HQTY"]
-#     if temp_qty!=None:
-#         temp_qty = float(temp_qty)
-#         qty_holding = max(temp_qty, qty_holding)
-#     temp_charg = items["CHARG"]
-#     if temp_charg!=None:
-#         charg = temp_charg
-#     temp_uom = items["MEINS"]
-#     if temp_uom!=None:
-#         uom = temp_uom        
-# else:
-#     for item in items:
-#         temp_qty = item["ATP_QTY"]
-#         if temp_qty!=None:
-#             temp_qty = float(temp_qty)
-#             qty = max(temp_qty, qty)
-#         temp_qty = item["HQTY"]
-#         if temp_qty!=None:
-#             temp_qty = float(temp_qty)
-#             qty_holding = max(temp_qty, qty_holding)
-#         temp_charg = item["CHARG"]
-#         if temp_charg!=None:
-#             charg = temp_charg
-#         temp_uom = item["MEINS"]
-#         if temp_uom!=None:
-#             uom = temp_uom
+if isinstance(items, dict):
+    temp_qty = items["ATP_QTY"]
+    if temp_qty!=None:
+        temp_qty = float(temp_qty)
+        qty = max(temp_qty, qty)
+    temp_qty = items["HQTY"]
+    if temp_qty!=None:
+        temp_qty = float(temp_qty)
+        qty_holding = max(temp_qty, qty_holding)
+    temp_charg = items["CHARG"]
+    if temp_charg!=None:
+        charg = temp_charg
+    temp_uom = items["MEINS"]
+    if temp_uom!=None:
+        uom = temp_uom        
+else:
+    for item in items:
+        temp_qty = item["ATP_QTY"]
+        if temp_qty!=None:
+            temp_qty = float(temp_qty)
+            qty = max(temp_qty, qty)
+        temp_qty = item["HQTY"]
+        if temp_qty!=None:
+            temp_qty = float(temp_qty)
+            qty_holding = max(temp_qty, qty_holding)
+        temp_charg = item["CHARG"]
+        if temp_charg!=None:
+            charg = temp_charg
+        temp_uom = item["MEINS"]
+        if temp_uom!=None:
+            uom = temp_uom
 
-# qty_holding = "5"
+print("Before : ")
+print(qty)
+print(qty_holding)
+print(charg)
+print(uom)
+print()
+print()
+print()
 
-# headers = {'content-type':'text/xml','accept':'application/json','cache-control':'no-cache'}
-# credentials = ("MOBSERVICE", "~lDT8+QklV=(")
+qty_holding = "5"
 
-# body = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:sap-com:document:sap:rfc:functions">
-#             <soapenv:Header/>
-#             <soapenv:Body>
-#             <urn:ZAPP_HOLDING_SO>
-#              <IM_AUART/>
-#              <IM_DATE/>
-#              <IM_EXTRA/>
-#              <IM_FLAG/>
-#              <IM_ID/>
-#              <IM_KUNNR>"""+ customer_id + """</IM_KUNNR>
-#              <IM_PERNR/>
-#              <IM_PO_NUMBER/>
-#              <IM_SPART/>
-#              <IM_VKORG>""" + company_code +"""</IM_VKORG>
-#              <IM_VTWEG/>
-#              <T_ITEM>
-#               <item>
-#                <MATKL></MATKL>
-#                <MATNR>"""+ product_id + """</MATNR>
-#                <ITEM></ITEM>
-#                <MAKTX></MAKTX>
-#                <QTY>"""+ qty_holding + """</QTY>
-#                <UOM>"""+ uom + """</UOM>
-#                <PRICE></PRICE>
-#                <INDPRICE></INDPRICE>
-#                <DISC></DISC>
-#                <INDDISC></INDDISC>
-#                <CHARG>""" + charg +"""</CHARG>
-#                <MO_PRICE></MO_PRICE>
-#                <NO_STOCK_IND></NO_STOCK_IND>
-#                <NO_STOCK_FOC></NO_STOCK_FOC>
-#                <FOC_ITEM></FOC_ITEM>
-#                <FOC_QTY></FOC_QTY>
-#                <FOC_UOM></FOC_UOM>
-#                <FOC_CHARG></FOC_CHARG>
-#                <PRC_DIFF_IND></PRC_DIFF_IND>
-#                <PRC_DIFF_NEW></PRC_DIFF_NEW>
-#                <SPCL_TEXT></SPCL_TEXT>
-#                <FOC_STD></FOC_STD>
-#                <FOC_ART></FOC_ART>
-#                <FOC_MCL></FOC_MCL>
-#                <INDICATOR1></INDICATOR1>
-#                <INDICATOR2></INDICATOR2>
-#                <TEXT1></TEXT1>
-#                <TEXT2></TEXT2>
-#                <CHARG_LIST></CHARG_LIST>
-#                <PRICE_CHANGE></PRICE_CHANGE>
-#                <FRM_ATP></FRM_ATP>
-#               </item>
-#              </T_ITEM>
-#              <T_MESSAGE>
-#               <item>
-#                <VBELN></VBELN>
-#                <TYPE></TYPE>
-#                <ID></ID>
-#                <NUMBER></NUMBER>
-#                <MESSAGE></MESSAGE>
-#                <LOG_NO></LOG_NO>
-#                <LOG_MSG_NO></LOG_MSG_NO>
-#                <MESSAGE_V1></MESSAGE_V1>
-#                <MESSAGE_V2></MESSAGE_V2>
-#                <MESSAGE_V3></MESSAGE_V3>
-#                <MESSAGE_V4></MESSAGE_V4>
-#                <PARAMETER></PARAMETER>
-#                <ROW></ROW>
-#                <FIELD></FIELD>
-#                <SYSTEM></SYSTEM>
-#               </item>
-#              </T_MESSAGE>
-#             </urn:ZAPP_HOLDING_SO>
-#             </soapenv:Body>
-#             </soapenv:Envelope>"""
+headers = {'content-type':'text/xml','accept':'application/json','cache-control':'no-cache'}
+credentials = ("MOBSERVICE", "~lDT8+QklV=(")
 
-# print(body)
+body = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:sap-com:document:sap:rfc:functions">
+            <soapenv:Header/>
+            <soapenv:Body>
+            <urn:ZAPP_HOLDING_SO>
+             <IM_AUART/>
+             <IM_DATE/>
+             <IM_EXTRA/>
+             <IM_FLAG/>
+             <IM_ID/>
+             <IM_KUNNR>"""+ customer_id + """</IM_KUNNR>
+             <IM_PERNR/>
+             <IM_PO_NUMBER/>
+             <IM_SPART/>
+             <IM_VKORG>""" + company_code +"""</IM_VKORG>
+             <IM_VTWEG/>
+             <T_ITEM>
+              <item>
+               <MATKL></MATKL>
+               <MATNR>"""+ product_id + """</MATNR>
+               <ITEM></ITEM>
+               <MAKTX></MAKTX>
+               <QTY>"""+ qty_holding + """</QTY>
+               <UOM>"""+ uom + """</UOM>
+               <PRICE></PRICE>
+               <INDPRICE></INDPRICE>
+               <DISC></DISC>
+               <INDDISC></INDDISC>
+               <CHARG>""" + charg +"""</CHARG>
+               <MO_PRICE></MO_PRICE>
+               <NO_STOCK_IND></NO_STOCK_IND>
+               <NO_STOCK_FOC></NO_STOCK_FOC>
+               <FOC_ITEM></FOC_ITEM>
+               <FOC_QTY></FOC_QTY>
+               <FOC_UOM></FOC_UOM>
+               <FOC_CHARG></FOC_CHARG>
+               <PRC_DIFF_IND></PRC_DIFF_IND>
+               <PRC_DIFF_NEW></PRC_DIFF_NEW>
+               <SPCL_TEXT></SPCL_TEXT>
+               <FOC_STD></FOC_STD>
+               <FOC_ART></FOC_ART>
+               <FOC_MCL></FOC_MCL>
+               <INDICATOR1></INDICATOR1>
+               <INDICATOR2></INDICATOR2>
+               <TEXT1></TEXT1>
+               <TEXT2></TEXT2>
+               <CHARG_LIST></CHARG_LIST>
+               <PRICE_CHANGE></PRICE_CHANGE>
+               <FRM_ATP></FRM_ATP>
+              </item>
+             </T_ITEM>
+             <T_MESSAGE>
+              <item>
+               <VBELN></VBELN>
+               <TYPE></TYPE>
+               <ID></ID>
+               <NUMBER></NUMBER>
+               <MESSAGE></MESSAGE>
+               <LOG_NO></LOG_NO>
+               <LOG_MSG_NO></LOG_MSG_NO>
+               <MESSAGE_V1></MESSAGE_V1>
+               <MESSAGE_V2></MESSAGE_V2>
+               <MESSAGE_V3></MESSAGE_V3>
+               <MESSAGE_V4></MESSAGE_V4>
+               <PARAMETER></PARAMETER>
+               <ROW></ROW>
+               <FIELD></FIELD>
+               <SYSTEM></SYSTEM>
+              </item>
+             </T_MESSAGE>
+            </urn:ZAPP_HOLDING_SO>
+            </soapenv:Body>
+            </soapenv:Envelope>"""
 
-# holding_url = "http://s4hdev:8000/sap/bc/srt/rfc/sap/zser_holding_so/150/zser_holding_so/zbin_holding_so"
-# credentials = ("MOBSERVICE", "~lDT8+QklV=(")
+holding_url = "http://s4hdev:8000/sap/bc/srt/rfc/sap/zser_holding_so/150/zser_holding_so/zbin_holding_so"
+credentials = ("MOBSERVICE", "~lDT8+QklV=(")
 
-# import requests
-# import xmltodict
-# import json
+import requests
+import xmltodict
+import json
 
-# response = requests.get(url=holding_url,auth=credentials)
-# print(response.content)
+response = requests.get(url=holding_url,auth=credentials)
+print(response.content)
 
-# response_holding = requests.post(url=holding_url, auth=credentials, data=body, headers=headers)
-# content = response_holding.content
-# print(response_holding.status_code)
-# content = xmltodict.parse(content)
-# content = json.loads(json.dumps(content))
+response_holding = requests.post(url=holding_url, auth=credentials, data=body, headers=headers)
+content = response_holding.content
+print(response_holding.status_code)
+content = xmltodict.parse(content)
+content = json.loads(json.dumps(content))
 
-print(content)
+response = fetch_prices(product_id,company_code,test_url,customer_id)
+        
+items = response["soap-env:Envelope"]["soap-env:Body"]["n0:ZAPP_STOCK_PRICEResponse"]["T_DATA"]["item"]
+
+if isinstance(items, dict):
+    temp_qty = items["ATP_QTY"]
+    if temp_qty!=None:
+        temp_qty = float(temp_qty)
+        qty = max(temp_qty, qty)
+    temp_qty = items["HQTY"]
+    if temp_qty!=None:
+        temp_qty = float(temp_qty)
+        qty_holding = max(temp_qty, qty_holding)
+    temp_charg = items["CHARG"]
+    if temp_charg!=None:
+        charg = temp_charg
+    temp_uom = items["MEINS"]
+    if temp_uom!=None:
+        uom = temp_uom        
+else:
+    for item in items:
+        temp_qty = item["ATP_QTY"]
+        if temp_qty!=None:
+            temp_qty = float(temp_qty)
+            qty = max(temp_qty, qty)
+        temp_qty = item["HQTY"]
+        if temp_qty!=None:
+            temp_qty = float(temp_qty)
+            qty_holding = max(temp_qty, qty_holding)
+        temp_charg = item["CHARG"]
+        if temp_charg!=None:
+            charg = temp_charg
+        temp_uom = item["MEINS"]
+        if temp_uom!=None:
+            uom = temp_uom
+
+print("After : ")
+print(qty)
+print(qty_holding)
+print(charg)
+print(uom)
+print()
+print()
+print()
