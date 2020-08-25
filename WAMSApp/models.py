072b263539a656c1f22f9b932c00d892f38696e9
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2020-08-25 12:22:47 +0530

    channel product pull auto

--- WAMSApp/models.py
+++ WAMSApp/models.py
@@ -35,7 +35,7 @@ noon_product_json = {
     "created_date" : "",
     "status" : "Inactive",
     "http_link": "",
-    "now_price":0.0,
+    "was_price":0.0,
     "sale_price":0.0,
     "sale_start":"",
     "sale_end":"",
@@ -691,6 +691,38 @@ class Product(models.Model):
 
         if self.channel_product == None:
             channel_product_obj = ChannelProduct.objects.create()
+
+            noon_product_json_temp = channel_product_obj.noon_product_json
+            amazon_uk_product_json_temp = channel_product_obj.amazon_uk_product_json
+            amazon_uae_product_json_temp = channel_product_obj.amazon_uae_product_json
+            ebay_product_json_temp = channel_product_obj.ebay_product_json
+
+            noon_product_json_temp["product_name"] = self.product_name
+            noon_product_json_temp["product_description"] = self.product_description
+            noon_product_json_temp["category"] = "" if self.base_product.category==None else str(self.base_product.category)
+            noon_product_json_temp["sub_category"] = "" if self.base_product.sub_category==None else str(self.base_product.sub_category)
+
+            amazon_uk_product_json_temp["product_name"] = self.product_name
+            amazon_uk_product_json_temp["product_description"] = self.product_description
+            amazon_uk_product_json_temp["category"] = "" if self.base_product.category==None else str(self.base_product.category)
+            amazon_uk_product_json_temp["sub_category"] = "" if self.base_product.sub_category==None else str(self.base_product.sub_category)
+
+            amazon_uae_product_json_temp["product_name"] = self.product_name
+            amazon_uae_product_json_temp["product_description"] = self.product_description
+            amazon_uae_product_json_temp["category"] = "" if self.base_product.category==None else str(self.base_product.category)
+            amazon_uae_product_json_temp["sub_category"] = "" if self.base_product.sub_category==None else str(self.base_product.sub_category)
+
+            ebay_product_json_temp["product_name"] = self.product_name
+            ebay_product_json_temp["product_description"] = self.product_description
+            ebay_product_json_temp["category"] = "" if self.base_product.category==None else str(self.base_product.category)
+            ebay_product_json_temp["sub_category"] = "" if self.base_product.sub_category==None else str(self.base_product.sub_category)
+
+            channel_product_obj.noon_product_json = json.dumps(noon_product_json_temp)
+            channel_product_obj.amazon_uk_product_json = json.dumps(amazon_uk_product_json_temp)
+            channel_product_obj.amazon_uae_product_json = json.dumps(amazon_uae_product_json_temp)
+            channel_product_obj.ebay_product_json = json.dumps(ebay_product_json_temp)
+            channel_product_obj.save()
+
             self.channel_product = channel_product_obj
 
         if self.product_id != None and self.product_id != "":
