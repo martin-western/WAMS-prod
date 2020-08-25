3cfc43740329cbbb94d8d333a501812ad7227e77
raj1238 <shahrajyogesh@gmail.com>
2020-08-25 17:20:20 +0530

    SAP Automation 25

--- WAMSApp/urls_SAP.py
+++ WAMSApp/urls_SAP.py
@@ -6,37 +6,7 @@ from django.http import HttpResponseRedirect
 
 urlpatterns = [
 
-############################## MWS URLS #########################################
-
-############ Amazon UK ###############
-
-    url(r'^push-products-amazon-uk/$',views.PushProductsAmazonUK),
-    url(r'^push-products-inventory-amazon-uk/$',views.PushProductsInventoryAmazonUK),
-    url(r'^push-products-price-amazon-uk/$',views.PushProductsPriceAmazonUK),
-    url(r'^get-matching-products-amazon-uk/$',views.GetMatchingProductsAmazonUK),
-    url(r'^get-pricing-of-products-amazon-uk/$',views.GetPricingProductsAmazonUK),
-    url(r'^get-product-inventory-amazon-uk/$',views.GetProductInventoryAmazonUK),
-    url(r'^fetch-price-and-stock-periodically-amazon-uk',views.FetchPriceAndStockAmazonUK), 
-    url(r'^push-product-images-amazon-uk',views.PushProductImagesAmazonUK),
-
-########### Amazon UAE ###############
-
-    url(r'^push-products-amazon-uae/$',views.PushProductsAmazonUAE),
-    url(r'^push-products-inventory-amazon-uae/$',views.PushProductsInventoryAmazonUAE),
-    url(r'^push-products-price-amazon-uae/$',views.PushProductsPriceAmazonUAE),
-    url(r'^get-matching-products-amazon-uae/$',views.GetMatchingProductsAmazonUAE),
-    url(r'^get-pricing-of-products-amazon-uae/$',views.GetPricingProductsAmazonUAE),
-    url(r'^get-product-inventory-amazon-uae/$',views.GetProductInventoryAmazonUAE),
-    url(r'^fetch-price-and-stock-periodically-amazon-uae',views.FetchPriceAndStockAmazonUAE), 
-    url(r'^push-product-images-amazon-uae',views.PushProductImagesAmazonUAE),
-
-########## Feed Reports ##############
-
-    url(r'^fetch-report-list/$',views.FetchReportList),
-    url(r'^fetch-report-details/$',views.FetchReportDetails),
-    url(r'^refresh-report-status/$',views.RefreshReportStatus),
-
-######### Orders #####################
+############################## SAP Automation #########################################
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
