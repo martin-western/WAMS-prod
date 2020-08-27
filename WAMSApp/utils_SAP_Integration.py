9fabd7826e9bb98ce169d22d4f03b3b9facf8e75
raj1238 <shahrajyogesh@gmail.com>
2020-08-27 16:27:43 +0530

    SAP Automation 61

--- WAMSApp/utils_SAP_Integration.py
+++ WAMSApp/utils_SAP_Integration.py
@@ -62,6 +62,63 @@ def xml_generator_for_price_and_stock_SAP(seller_sku,company_code,customer_id)
         logger.error("xml_generator_for_price_and_stock_SAP: %s at %s", str(e), str(exc_tb.tb_lineno))
         return []
 
+def xml_generator_for_price_and_stock_SAP(seller_sku,company_code,customer_id)
+
+    try :
+
+        xml_feed = """<soapenv:Envelope xmlns:urn="urn:sap-com:document:sap:rfc:functions" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+                    <soapenv:Header />
+                    <soapenv:Body>
+                    <urn:ZAPP_STOCK_PRICE>
+                    <IM_KUNNR>"""+ customer_id +"""</IM_KUNNR>
+                    <IM_MATNR>
+                    <item>
+                     <MATNR>""" + seller_sku + """</MATNR>
+                    </item>
+                    </IM_MATNR>
+                    <IM_VKORG>
+                    <item>
+                     <VKORG>""" + company_code + """</VKORG>
+                    </item>
+                    </IM_VKORG>
+                    <T_DATA>
+                    <item>
+                     <MATNR></MATNR>
+                     <MAKTX></MAKTX>
+                     <LGORT></LGORT>
+                     <CHARG></CHARG>
+                     <SPART></SPART>
+                     <MEINS></MEINS>
+                     <ATP_QTY></ATP_QTY>
+                     <TOT_QTY></TOT_QTY>
+                     <CURRENCY></CURRENCY>
+                     <IC_EA></IC_EA>
+                     <OD_EA></OD_EA>
+                     <EX_EA></EX_EA>
+                     <RET_EA></RET_EA>
+                     <WERKS></WERKS>
+                     <WERKS></WERKS>
+                     <WWGHA1></WWGHA1>
+                     <WWGHB1></WWGHB1>
+                     <WWGHA2></WWGHA2>
+                     <WWGHB2></WWGHB2>
+                     <WWGHA3></WWGHA3>
+                     <WWGHB3></WWGHB3>
+                     <HQTY></HQTY>
+                    </item>
+                    </T_DATA>
+                    </urn:ZAPP_STOCK_PRICE>
+                    </soapenv:Body>
+                    </soapenv:Envelope>"""
+
+        return xml_feed
+
+    except Exception as e:
+        
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        logger.error("xml_generator_for_price_and_stock_SAP: %s at %s", str(e), str(exc_tb.tb_lineno))
+        return []
+
 def fetch_prices_and_stock(seller_sku,company_code,url,customer_id):
     
     try:
