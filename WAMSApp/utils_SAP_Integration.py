3d2ef38ca19188f1e23e5f7710fc3aab61044eb2
raj1238 <shahrajyogesh@gmail.com>
2020-08-27 16:37:32 +0530

    SAP Automation 64

--- WAMSApp/utils_SAP_Integration.py
+++ WAMSApp/utils_SAP_Integration.py
@@ -169,6 +169,113 @@ def xml_generator_for_holding_tansfer(seller_sku,company_code,customer_id,transf
         logger.error("xml_generator_for_holding_tansfer: %s at %s", str(e), str(exc_tb.tb_lineno))
         return []
 
+def xml_generator_for_intercompany_tansfer(seller_sku,company_code,customer_id,transfer_information)
+
+    try :
+
+        xml_feed = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:sap-com:document:sap:rfc:functions">
+            <soapenv:Header/>
+            <soapenv:Body>
+            <urn:ZAPP_HOLDING_SO>
+             <IM_AUART/>
+             <IM_DATE/>
+             <IM_EXTRA/>
+             <IM_FLAG/>
+             <IM_ID/>
+             <IM_KUNNR>"""+ customer_id + """</IM_KUNNR>
+             <IM_PERNR/>
+             <IM_PO_NUMBER/>
+             <IM_SPART/>
+             <IM_VKORG>""" + company_code +"""</IM_VKORG>
+             <IM_VTWEG/>
+             <T_ITEM>"""
+
+        msg_feed="<T_MESSAGE>"
+        for item in transfer_information:
+            xml_feed+="""
+                  <item>
+                   <MATKL></MATKL>
+                   <MATNR>"""+ seller_sku + """</MATNR>
+                   <ITEM></ITEM>
+                   <MAKTX></MAKTX>
+                   <QTY>"""+ str(item["qty"]) + """</QTY>
+                   <UOM>"""+ str(item["uom"]) + """</UOM>
+                   <PRICE></PRICE>
+                   <INDPRICE></INDPRICE>
+                   <DISC></DISC>
+                   <INDDISC></INDDISC>
+                   <CHARG>"""+ str(item["batch"]) + """</CHARG>
+                   <MO_PRICE></MO_PRICE>
+                   <NO_STOCK_IND></NO_STOCK_IND>
+                   <NO_STOCK_FOC></NO_STOCK_FOC>
+                   <FOC_ITEM></FOC_ITEM>
+                   <FOC_QTY></FOC_QTY>
+                   <FOC_UOM></FOC_UOM>
+                   <FOC_CHARG></FOC_CHARG>
+                   <PRC_DIFF_IND></PRC_DIFF_IND>
+                   <PRC_DIFF_NEW></PRC_DIFF_NEW>
+                   <SPCL_TEXT></SPCL_TEXT>
+                   <FOC_STD></FOC_STD>
+                   <FOC_ART></FOC_ART>
+                   <FOC_MCL></FOC_MCL>
+                   <INDICATOR1></INDICATOR1>
+                   <INDICATOR2></INDICATOR2>
+                   <TEXT1></TEXT1>
+                   <TEXT2></TEXT2>
+                   <CHARG_LIST></CHARG_LIST>
+                   <PRICE_CHANGE></PRICE_CHANGE>
+                   <FRM_ATP></FRM_ATP>
+                  </item>"""
+            msg_feed+="""<item>
+               <VBELN></VBELN>
+               <TYPE></TYPE>
+               <ID></ID>
+               <NUMBER></NUMBER>
+               <MESSAGE></MESSAGE>
+               <LOG_NO></LOG_NO>
+               <LOG_MSG_NO></LOG_MSG_NO>
+               <MESSAGE_V1></MESSAGE_V1>
+               <MESSAGE_V2></MESSAGE_V2>
+               <MESSAGE_V3></MESSAGE_V3>
+               <MESSAGE_V4></MESSAGE_V4>
+               <PARAMETER></PARAMETER>
+               <ROW></ROW>
+               <FIELD></FIELD>
+               <SYSTEM></SYSTEM>
+              </item>
+              <item>
+               <VBELN></VBELN>
+               <TYPE></TYPE>
+               <ID></ID>
+               <NUMBER></NUMBER>
+               <MESSAGE></MESSAGE>
+               <LOG_NO></LOG_NO>
+               <LOG_MSG_NO></LOG_MSG_NO>
+               <MESSAGE_V1></MESSAGE_V1>
+               <MESSAGE_V2></MESSAGE_V2>
+               <MESSAGE_V3></MESSAGE_V3>
+               <MESSAGE_V4></MESSAGE_V4>
+               <PARAMETER></PARAMETER>
+               <ROW></ROW>
+               <FIELD></FIELD>
+               <SYSTEM></SYSTEM>
+              </item>"""
+              
+        xml_feed+="</T_ITEM>"
+        msg_feed+="</T_MESSAGE>"
+
+        xml_feed+=msg_feed+"""</urn:ZAPP_HOLDING_SO>
+                                </soapenv:Body>
+                                </soapenv:Envelope>"""
+
+        return xml_feed
+
+    except Exception as e:
+        
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        logger.error("xml_generator_for_intercompany_tansfer: %s at %s", str(e), str(exc_tb.tb_lineno))
+        return []
+
 def fetch_prices_and_stock(seller_sku,company_code,url,customer_id):
     
     try:
