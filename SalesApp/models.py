226ae8f327268a1c788fc81e190a7271e3c1b70d
raj1238 <shahrajyogesh@gmail.com>
2020-10-15 16:23:07 +0530

    SalesApp Integration 21

--- SalesApp/models.py
+++ SalesApp/models.py
@@ -17,38 +17,15 @@ from django.core.cache import cache
 
 logger = logging.getLogger(__name__)
 
-class Notification(models.Model):
-	
-    image = models.ForeignKey('wamsapp.image', null=True, blank=True, on_delete=models.CASCADE)
-    uuid = models.CharField(max_length=200, default="",blank=True,null=True)
-    title = models.CharField(max_length=200, default="",blank=True,null=True)
-    message = models.CharField(max_length=200, default="",blank=True,null=True)
-    
-    def save(self, *args, **kwargs):
-        
-        if self.pk == None:
-            self.uuid = uuid.uuid4()
-        
-        super(Notification, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = "Notification"
-        verbose_name_plural = "Notifications"
-
 class SalesAppUser(User):
 
-    image = models.ForeignKey('wamsapp.image', null=True, blank=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, blank=True, on_delete=models.CASCADE)
     customer_id = models.CharField(max_length=200, default="",blank=True,null=True)
-    first_name = models.CharField(max_length=200, default="",blank=True,null=True)
-    last_name = models.CharField(max_length=200, default="",blank=True,null=True)
+    fcm_id = models.TextField(default="{}")
     email_id = models.CharField(max_length=200, default="",blank=True,null=True)
     contact_number = models.CharField(max_length=200, default="",blank=True,null=True)
     country = models.CharField(max_length=200, default="",blank=True,null=True)
-    favourite_products = models.ManyToManyField('wamsapp.product',blank=True)
-    notifications = models.ManyToManyField(Notification,blank=True)
+    favourite_products = models.ManyToManyField(Product,blank=True)
     
     def save(self, *args, **kwargs):
         
