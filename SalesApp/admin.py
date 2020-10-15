2c3ae0e2b91b6d1f771f0ebe98d474f1ebed448a
raj1238 <shahrajyogesh@gmail.com>
2020-10-15 15:54:57 +0530

    SalesApp Integration 6

--- SalesApp/admin.py
+++ SalesApp/admin.py
@@ -3,50 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from dealshub.models import *
+from SalesApp.models import *
 
 
-class DealsHubProductAdmin(admin.ModelAdmin):
-    list_display = ('product',)
+# class DealsHubProductAdmin(admin.ModelAdmin):
+#     list_display = ('product',)
 
-
-class DealshubAdminSectionOrderAdmin(admin.ModelAdmin):
-    list_display = ('dealshub_banner_index', 'homepage_schedular_index', 'full_banner_ad_index', 'category_grid_banner_index')
-
-
-class SectionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'order_index')
-
-
-class BannerTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'limit', 'website_group')
-
-
-class DealsHubUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'contact_verified', 'verification_code', 'date_created')
-
-
-class CartAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'merchant_reference')
-
-class FastCartAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'merchant_reference')
-
-
-admin.site.register(DealsHubProduct, DealsHubProductAdmin)
-admin.site.register(Section, SectionAdmin)
-admin.site.register(BannerType, BannerTypeAdmin)
-admin.site.register(Banner)
-admin.site.register(UnitBannerImage)
-admin.site.register(Address)
-admin.site.register(UnitCart)
-admin.site.register(Cart, CartAdmin)
-admin.site.register(FastCart, FastCartAdmin)
-admin.site.register(DealsHubUser, DealsHubUserAdmin)
-admin.site.register(UnitOrder)
-admin.site.register(Order)
-admin.site.register(Location)
-admin.site.register(LocationGroup)
-admin.site.register(Voucher)
-admin.site.register(WishList)
-admin.site.register(UnitWishList)
+# admin.site.register(DealsHubProduct, DealsHubProductAdmin)
