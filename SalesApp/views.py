84090e575ac10522d16250df784495f741afb2fa
raj1238 <shahrajyogesh@gmail.com>
2020-10-18 18:17:44 +0530

    SalesApp Integration 58

--- SalesApp/views.py
+++ SalesApp/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 
 from WAMSApp.models import *
+from WAMSApp.constants import *
 from SalesApp.models import *
 from SalesApp.utils import *
-from SalesApp.constants import *
 
 from django.shortcuts import HttpResponse, get_object_or_404
 from django.contrib.auth import logout, authenticate, login
