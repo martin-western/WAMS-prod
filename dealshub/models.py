8e4fb80dd6ada3e81934f19ca874dba49b969f46
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2020-10-26 16:56:26 +0530

    dealshub specific name

--- dealshub/models.py
+++ dealshub/models.py
@@ -150,6 +150,8 @@ class DealsHubProductRecoveryManager(models.Manager):
 class DealsHubProduct(models.Model):
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True)
+    product_name = models.CharField(max_length=200, default="")
+    product_description = models.TextField(default="", blank=True)
     was_price = models.FloatField(default=0)
     now_price = models.FloatField(default=0)
     promotional_price = models.FloatField(default=0)
