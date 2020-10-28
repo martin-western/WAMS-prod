c4ba0da19fe369483292cb2de8b8b8d3be8c8240
raj1238 <shahrajyogesh@gmail.com>
2020-10-28 19:13:07 +0530

    SalesApp Integration 112

--- SalesApp/utils.py
+++ SalesApp/utils.py
@@ -21,6 +21,8 @@ from django.template import loader
 
 logger = logging.getLogger(__name__)
 
+SERVER_TOKEN = "AAAARH4JrjU:APA91bEi1uAlKWWMNnX7pCwXs2RBoAnLahZ0MAT92jOp80lE25Cbjn03iXHxNZeNvuMlYZYYKjXtmw21rz8IJEEGYHUS8khTiNzUgn61x_HFBQM78Q9QUNismYfmsc8B8WVrWRkTlX8S"
+
 def convert_to_ascii(s):
     
     s = s.replace(u'\u2013', "-").replace(u'\u2019', "'").replace(u'\u2018', "'").replace(u'\u201d','"').replace(u'\u201c','"')
@@ -36,9 +38,27 @@ def send_firebase_notifications(fcm_ids,notification_info):
 
 	registration_ids = fcm_ids
 	message_title = notification_info["title"]
+	message_subtitle = notification_info["subtitle"]
 	message_body = notification_info["body"]
 	message_image = notification_info["image"]
-	message_body = "Hope you're having fun this weekend, don't forget to check today's news"
-	result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+	
+	headers = {
+	        'Content-Type': 'application/json',
+	        'Authorization': 'key=' + SERVER_TOKEN,
+	      }
 
-	print(result)
+	body = {
+	          'notification': {
+	          					"title": "Sample Title for notification -OmnyComm 5",
+	                            "subtitle" : "Sampe Subtitle for notification - OmnyComm 5",
+	                            "body" : "Sampe Body for notification - OmnyComm 5",
+	                            "image" : "https://cdn.omnycomm.com/l.jpeg"
+	                            },
+	          'registration_ids':
+	              registration_ids,
+	          'priority': 'high',
+	        #   'data': dataPayLoad,
+	        }
+	response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headers, data=json.dumps(body))
+
+	return response
