8e8358f17a6611c7e9cf2b2fb13bb0fac3a45f12
raj1238 <shahrajyogesh@gmail.com>
2020-10-28 15:34:32 +0530

    SalesApp Integration 82

--- SalesApp/admin.py
+++ SalesApp/admin.py
@@ -9,9 +9,9 @@ from SalesApp.models import *
 class SalesAppUserAdmin(admin.ModelAdmin):
     list_display = ('username',)
 
-class SalesAppUserAdmin(admin.ModelAdmin):
-    list_display = ('username',)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('title',)
 
 admin.site.register(SalesAppUser, SalesAppUserAdmin)
 
-admin.site.register(SalesAppUser, SalesAppUserAdmin)
+admin.site.register(Notification, NotificationAdmin)
