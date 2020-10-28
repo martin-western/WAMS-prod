eb66cd9c704815c2604861841880cdb280abb0d0
raj1238 <shahrajyogesh@gmail.com>
2020-10-28 16:32:26 +0530

    SalesApp Integration 92

--- SalesApp/views.py
+++ SalesApp/views.py
@@ -240,7 +240,7 @@ class SearchProductByBrandAPI(APIView):
             response["product_list"] = product_list
             response["total_pages"] = total_pages
             response['status'] = 200
-            response['message'] = "Successfull"
+            response['message'] = "Successful"
         
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -378,7 +378,7 @@ class FetchFavouriteProductsAPI(APIView):
             response["product_list"] = product_list
             response["total_pages"] = total_pages
             response['status'] = 200
-            response['message'] = "Successfull"
+            response['message'] = "Successful"
         
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -409,6 +409,77 @@ class CreateNotification(APIView):
             expiry_date = data.get('expiry_date', "")
 
             
+            response['status'] = 200
+            response['message'] = "Successful"
+        
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logger.error("CreateNotification: %s at %s", e, str(exc_tb.tb_lineno))
+
+        return Response(data=response)
+
+class FetchFavouriteProductsAPI(APIView):
+
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def post(self, request, *args, **kwargs):
+
+        response = {}
+        response['status'] = 500
+        
+        try:
+            
+            data = request.data
+            logger.info("FetchFavouriteProductsAPI: %s", str(data))
+
+            if not isinstance(data, dict):
+                data = json.loads(data)
+
+            page = int(data.get('page', 1))
+
+            try :
+                sales_user_obj = SalesAppUser.objects.get(username=request.user.username)
+            except Exception as e :
+                response['status'] = 403
+                response['message'] = "User not Logged In"
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                logger.warning("FetchFavouriteProductsAPI: %s at %s", e, str(exc_tb.tb_lineno))
+                return Response(data=response)
+
+            product_objs = sales_user_obj.favourite_products.all()
+
+            paginator = Paginator(product_objs, 20)
+            total_pages = paginator.num_pages
+            
+            if page > total_pages:
+                response['status'] = 404
+                response['message'] = "Page number out of range"
+                logger.warning("FetchFavouriteProductsAPI : Page number out of range")
+                return Response(data=response)
+
+            page_product_objs = paginator.page(page)
+
+            product_list = []
+            
+            for product_obj in page_product_objs:
+                
+                try:
+                    
+                    temp_dict = {}
+                    temp_dict["product_name"] = product_obj.product_name
+                    temp_dict["image_url"] = product_obj.get_display_image_url()
+                    temp_dict["product_description"] = product_obj.product_description
+                    temp_dict["seller_sku"] = product_obj.base_product.seller_sku
+                    temp_dict["product_id"] = "" if product_obj.product_id==None else str(product_obj.product_id)
+                    
+                    product_list.append(temp_dict)
+                
+                except Exception as e:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    logger.error("FetchFavouriteProductsAPI: %s at %s", e, str(exc_tb.tb_lineno))
+
+            response["product_list"] = product_list
+            response["total_pages"] = total_pages
             response['status'] = 200
             response['message'] = "Successful"
         
@@ -481,7 +552,7 @@ class FetchFavouriteProductsAPI(APIView):
             response["product_list"] = product_list
             response["total_pages"] = total_pages
             response['status'] = 200
-            response['message'] = "Successfull"
+            response['message'] = "Successful"
         
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -552,78 +623,7 @@ class FetchFavouriteProductsAPI(APIView):
             response["product_list"] = product_list
             response["total_pages"] = total_pages
             response['status'] = 200
-            response['message'] = "Successfull"
-        
-        except Exception as e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            logger.error("FetchFavouriteProductsAPI: %s at %s", e, str(exc_tb.tb_lineno))
-
-        return Response(data=response)
-
-class FetchFavouriteProductsAPI(APIView):
-
-    authentication_classes = (CsrfExemptSessionAuthentication,)
-
-    def post(self, request, *args, **kwargs):
-
-        response = {}
-        response['status'] = 500
-        
-        try:
-            
-            data = request.data
-            logger.info("FetchFavouriteProductsAPI: %s", str(data))
-
-            if not isinstance(data, dict):
-                data = json.loads(data)
-
-            page = int(data.get('page', 1))
-
-            try :
-                sales_user_obj = SalesAppUser.objects.get(username=request.user.username)
-            except Exception as e :
-                response['status'] = 403
-                response['message'] = "User not Logged In"
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                logger.warning("FetchFavouriteProductsAPI: %s at %s", e, str(exc_tb.tb_lineno))
-                return Response(data=response)
-
-            product_objs = sales_user_obj.favourite_products.all()
-
-            paginator = Paginator(product_objs, 20)
-            total_pages = paginator.num_pages
-            
-            if page > total_pages:
-                response['status'] = 404
-                response['message'] = "Page number out of range"
-                logger.warning("FetchFavouriteProductsAPI : Page number out of range")
-                return Response(data=response)
-
-            page_product_objs = paginator.page(page)
-
-            product_list = []
-            
-            for product_obj in page_product_objs:
-                
-                try:
-                    
-                    temp_dict = {}
-                    temp_dict["product_name"] = product_obj.product_name
-                    temp_dict["image_url"] = product_obj.get_display_image_url()
-                    temp_dict["product_description"] = product_obj.product_description
-                    temp_dict["seller_sku"] = product_obj.base_product.seller_sku
-                    temp_dict["product_id"] = "" if product_obj.product_id==None else str(product_obj.product_id)
-                    
-                    product_list.append(temp_dict)
-                
-                except Exception as e:
-                    exc_type, exc_obj, exc_tb = sys.exc_info()
-                    logger.error("FetchFavouriteProductsAPI: %s at %s", e, str(exc_tb.tb_lineno))
-
-            response["product_list"] = product_list
-            response["total_pages"] = total_pages
-            response['status'] = 200
-            response['message'] = "Successfull"
+            response['message'] = "Successful"
         
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
