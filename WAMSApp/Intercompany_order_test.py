476f2cc1da704396ac8ecf932e2ad68e1da8b9c8
raj1238 <shahrajyogesh@gmail.com>
2020-11-10 13:08:19 +0530

    SAP Automation 307

--- WAMSApp/Intercompany_order_test.py
+++ WAMSApp/Intercompany_order_test.py
@@ -4,8 +4,8 @@ import json
 import uuid
 
 company_code = "1000"
-customer_id = "40000637"
-product_id = " GAF37503"
+customer_id = "40000195"
+product_id = "GK175"
 IP = "94.56.89.116"
 
 headers = {'content-type':'text/xml','accept':'application/json','cache-control':'no-cache'}
@@ -14,7 +14,7 @@ credentials = ("MOBSERVICE", "~lDT8+QklV=(")
 print(product_id)
 print()
 
-test_url = "http://wig.westernint.com:8000/sap/bc/srt/rfc/sap/zser_stock_price/300/zser_stock_price/zbin_stock_price"
+test_url = "http://94.56.89.116:8000/sap/bc/srt/rfc/sap/zser_stock_price/150/zser_stock_price/zbin_stock_price"
 # production_url="http://wig.westernint.com:8000/sap/bc/srt/rfc/sap/zser_stock_price/300/zser_stock_price/zbin_stock_price"
 
 def fetch_prices(product_id,company_code,url,customer_id):
@@ -131,7 +131,7 @@ print()
 
 ##################################
 
-qty_holding = 5.0
+qty_holding = 8.0
 
 body = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:sap-com:document:sap:rfc:functions">
             <soapenv:Header/>
@@ -151,7 +151,40 @@ body = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envel
              <T_ITEM>
               <item>
                <MATKL></MATKL>
-               <MATNR>"""+ product_id + """</MATNR>
+               <MATNR>GTR34</MATNR>
+               <ITEM></ITEM>
+               <MAKTX></MAKTX>
+               <QTY>"""+ str(qty_holding) + """</QTY>
+               <UOM>"""+ uom + """</UOM>
+               <PRICE></PRICE>
+               <INDPRICE></INDPRICE>
+               <DISC></DISC>
+               <INDDISC></INDDISC>
+               <CHARG></CHARG>
+               <MO_PRICE></MO_PRICE>
+               <NO_STOCK_IND></NO_STOCK_IND>
+               <NO_STOCK_FOC></NO_STOCK_FOC>
+               <FOC_ITEM></FOC_ITEM>
+               <FOC_QTY></FOC_QTY>
+               <FOC_UOM></FOC_UOM>
+               <FOC_CHARG></FOC_CHARG>
+               <PRC_DIFF_IND></PRC_DIFF_IND>
+               <PRC_DIFF_NEW></PRC_DIFF_NEW>
+               <SPCL_TEXT></SPCL_TEXT>
+               <FOC_STD></FOC_STD>
+               <FOC_ART></FOC_ART>
+               <FOC_MCL></FOC_MCL>
+               <INDICATOR1></INDICATOR1>
+               <INDICATOR2></INDICATOR2>
+               <TEXT1></TEXT1>
+               <TEXT2></TEXT2>
+               <CHARG_LIST></CHARG_LIST>
+               <PRICE_CHANGE></PRICE_CHANGE>
+               <FRM_ATP></FRM_ATP>
+              </item>
+              <item>
+               <MATKL></MATKL>
+               <MATNR>GESL4026</MATNR>
                <ITEM></ITEM>
                <MAKTX></MAKTX>
                <QTY>"""+ str(qty_holding) + """</QTY>
@@ -201,12 +234,29 @@ body = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envel
                <FIELD></FIELD>
                <SYSTEM></SYSTEM>
               </item>
+              <item>
+               <VBELN></VBELN>
+               <TYPE></TYPE>
+               <ID></ID>
+               <NUMBER></NUMBER>
+               <MESSAGE></MESSAGE>
+               <LOG_NO></LOG_NO>
+               <LOG_MSG_NO></LOG_MSG_NO>
+               <MESSAGE_V1></MESSAGE_V1>
+               <MESSAGE_V2></MESSAGE_V2>
+               <MESSAGE_V3></MESSAGE_V3>
+               <MESSAGE_V4></MESSAGE_V4>
+               <PARAMETER></PARAMETER>
+               <ROW></ROW>
+               <FIELD></FIELD>
+               <SYSTEM></SYSTEM>
+              </item>
              </T_MESSAGE>
             </urn:ZAPP_HOLDING_SO>
             </soapenv:Body>
             </soapenv:Envelope>"""
 
-holding_url = "http://wig.westernint.com:8000/sap/bc/srt/rfc/sap/zser_holding_so/300/zser_holding_so/zbin_holding_so"
+holding_url = "http://94.56.89.116:8000/sap/bc/srt/rfc/sap/zser_holding_so/150/zser_holding_so/zbin_holding_so"
 
 response_holding = requests.post(url=holding_url, auth=credentials, data=body, headers=headers)
 content = response_holding.content
@@ -217,48 +267,48 @@ items = content["soap-env:Envelope"]["soap-env:Body"]["n0:ZAPP_HOLDING_SORespons
 
 print(items)
 
-#################################
+# #################################
 
-###### After Holding ############
+# ###### After Holding ############
 
-################################
+# ################################
 
-response = fetch_prices(product_id,company_code,test_url,customer_id)
+# response = fetch_prices(product_id,company_code,test_url,customer_id)
         
-items = response["soap-env:Envelope"]["soap-env:Body"]["n0:ZAPP_STOCK_PRICEResponse"]["T_DATA"]["item"]
-uom = "EA"
-charg = None
-total_atp = 0.0
-total_holding = 0.0
-prices_stock_list = []
+# items = response["soap-env:Envelope"]["soap-env:Body"]["n0:ZAPP_STOCK_PRICEResponse"]["T_DATA"]["item"]
+# uom = "EA"
+# charg = None
+# total_atp = 0.0
+# total_holding = 0.0
+# prices_stock_list = []
 
-if isinstance(items, dict):
-    temp_dict={}
-    temp_dict["charg"] = items["CHARG"]
-    temp_dict["uom"] = items["MEINS"]    
-    temp_dict["atp_qty"] = float(items["ATP_QTY"])
-    total_atp = total_atp+float(items["ATP_QTY"])
-    temp_dict["qty_holding"] = float(items["HQTY"])
-    total_holding = total_holding + float(items["HQTY"])
-    prices_stock_list.append(temp_dict)
-else:
-    for item in items:
-        temp_dict={}
-        temp_dict["charg"] = item["CHARG"]
-        temp_dict["uom"] = item["MEINS"]    
-        temp_dict["atp_qty"] = float(item["ATP_QTY"])
-        total_atp = total_atp+float(item["ATP_QTY"])
-        temp_dict["qty_holding"] = float(item["HQTY"])
-        total_holding = total_holding + float(item["HQTY"])
-        prices_stock_list.append(temp_dict)
+# if isinstance(items, dict):
+#     temp_dict={}
+#     temp_dict["charg"] = items["CHARG"]
+#     temp_dict["uom"] = items["MEINS"]    
+#     temp_dict["atp_qty"] = float(items["ATP_QTY"])
+#     total_atp = total_atp+float(items["ATP_QTY"])
+#     temp_dict["qty_holding"] = float(items["HQTY"])
+#     total_holding = total_holding + float(items["HQTY"])
+#     prices_stock_list.append(temp_dict)
+# else:
+#     for item in items:
+#         temp_dict={}
+#         temp_dict["charg"] = item["CHARG"]
+#         temp_dict["uom"] = item["MEINS"]    
+#         temp_dict["atp_qty"] = float(item["ATP_QTY"])
+#         total_atp = total_atp+float(item["ATP_QTY"])
+#         temp_dict["qty_holding"] = float(item["HQTY"])
+#         total_holding = total_holding + float(item["HQTY"])
+#         prices_stock_list.append(temp_dict)
 
-print("After Holding : ")
-print("Batch"+'\t'+"UOM"+'\t'+"ATP"+'\t'+"Holding")
-for item in prices_stock_list:
-    if item["charg"] != None:
-        print(str(item["charg"])+'\t'+str(item["uom"])+'\t'+str(item["atp_qty"])+'\t'+str(item["qty_holding"]))
-print("Total"+'\t'+'\t'+str(total_atp)+'\t'+str(total_holding))
-print()
+# print("After Holding : ")
+# print("Batch"+'\t'+"UOM"+'\t'+"ATP"+'\t'+"Holding")
+# for item in prices_stock_list:
+#     if item["charg"] != None:
+#         print(str(item["charg"])+'\t'+str(item["uom"])+'\t'+str(item["atp_qty"])+'\t'+str(item["qty_holding"]))
+# print("Total"+'\t'+'\t'+str(total_atp)+'\t'+str(total_holding))
+# print()
 
 
 ###################################
