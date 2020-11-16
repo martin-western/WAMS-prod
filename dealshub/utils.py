c181319e5edc8e6f25ac04c4d7f6636efbe0baef
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2020-11-16 16:00:43 +0530

    new search

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -814,3 +814,13 @@ def get_all_the_charges(order_obj):
         exc_type, exc_obj, exc_tb = sys.exc_info()
         logger.error("get_all_the_charges: %s at %s", e, str(exc_tb.tb_lineno))
         return charges
+
+def remove_stopwords(string):
+    words = string.strip().split(" ")
+    stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+    cleaned_words = []
+    for word in words:
+        if word not in stopwords:
+            cleaned_words.append(word)
+    cleaned_string = " ".join(cleaned_words)
+    return cleaned_string
