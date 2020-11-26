84f924dff38b8249ba0f0272da5ecd64bcf3aeb5
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2020-11-26 10:29:38 +0530

    bug fixes

--- dealshub/views.py
+++ dealshub/views.py
@@ -1112,7 +1112,7 @@ class FetchWIGCategoriesAPI(APIView):
                 if super_category_name!="":
                     is_super_category_available = True
                     super_category_obj = SuperCategory.objects.get(name=super_category_name)
-                elif category_name!="":
+                elif category_name!="" and category_name.lower()!="all":
                     super_category_obj = Category.objects.filter(name=category_name)[0].super_category
                 elif subcategory_name!="":
                     super_category_obj = SubCategory.objects.filter(name=subcategory_name)[0].category.super_category
