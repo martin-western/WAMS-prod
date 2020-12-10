45f3148f966899c7b6427897e1750fdd44903487
mann27 <mannshah2712@gmail.com>
2020-12-10 13:51:15 +0530

    changed to exists()

--- dealshub/views_dh.py
+++ dealshub/views_dh.py
@@ -3919,7 +3919,7 @@ class FetchOrdersForWarehouseManagerAPI(APIView):
 
                     temp_dict["sapStatus"] = order_obj.sap_status
                     sap_warning_list = ["GRN Conflict", "Failed"]
-                    sap_warning = False if unit_order_objs.filter(order=order_obj, sap_status__in=sap_fail_list).count() == 0 else True
+                    sap_warning = False if unit_order_objs.filter(order=order_obj, sap_status__in=sap_fail_list).exists() else True
                     temp_dict["sapWarning"] = sap_warning
 
                     subtotal = order_obj.get_subtotal()
