2ca3167abdb14421939dbd7103baa5feeb6b0232
Sparsh <sparshgoil1999@gmail.com>
2021-01-03 19:32:50 +0530

    fix models

--- dealshub/models.py
+++ dealshub/models.py
@@ -1138,8 +1138,8 @@ class B2BUser(DealsHubUser):
     company_name = models.CharField(default="",max_length=50)
     interested_categories = models.ManyToManyField(Category,blank = True)
     vat_certificate = models.FileField(upload_to = 'vat_certificate',null=True, blank=True)
-    trade_license = model.FileField(upload_to = 'trade_license',null=True,blank=True)
-    passport_copy = model.FileField(upload_to = 'passport_copy',null=True,blank=True)
+    trade_license = models.FileField(upload_to = 'trade_license',null=True,blank=True)
+    passport_copy = models.FileField(upload_to = 'passport_copy',null=True,blank=True)
 
     STATUS_OPTIONS = (
         ('Pending','Pending'),
