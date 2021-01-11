bedd94b699a600b23c29c2c382cf4322d3d6dec4
mann27 <mannshah2712@gmail.com>
2021-01-11 13:57:08 +0530

    minor change

--- dealshub/views_dh.py
+++ dealshub/views_dh.py
@@ -1754,6 +1754,7 @@ class FetchOrderDetailsAPI(APIView):
             if is_voucher_applied:
                 response["voucherCode"] = voucher_obj.voucher_code
                 response["voucherDiscount"] = voucher_obj.get_voucher_discount(order_obj.get_subtotal())
+            response["shippingMethod"] = unit_order_objs[0].shipping_method
 
             address_obj = order_obj.shipping_address
             if address_obj==None:
