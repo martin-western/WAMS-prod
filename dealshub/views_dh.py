8d7dfeb6c047fcf4b2823addea21bab2c47d9301
Sparsh <sparshgoil1999@gmail.com>
2021-02-11 16:13:37 +0530

    fetch and update cohort of user

--- dealshub/views_dh.py
+++ dealshub/views_dh.py
@@ -2408,6 +2408,7 @@ class FetchCustomerDetailsAPI(APIView):
             temp_dict["contactNumber"] = dealshub_user_obj.contact_number
             if is_b2b == True:
                 b2b_user_obj = B2BUser.objects.get(username = dealshub_user_obj.username)
+                temp_dict["cohort"] = b2b_user_obj.cohort
                 temp_dict["companyName"] = b2b_user_obj.company_name
                 temp_dict["vatCertification"] = b2b_user_obj.vat_certificate.url
                 temp_dict["tradeLicense"] = b2b_user_obj.trade_license.url
@@ -2512,6 +2513,7 @@ class UpdateB2BCustomerStatusAPI(APIView):
             passport_copy_status = data["passportCopyStatus"]
             customer_name = data["customerName"]
             email_id = data["emailId"]
+            cohort = data["cohort"]
 
             b2b_user_obj.company_name = company_name
             b2b_user_obj.first_name = customer_name
@@ -2519,6 +2521,7 @@ class UpdateB2BCustomerStatusAPI(APIView):
             b2b_user_obj.vat_certificate_status = vat_certificate_status
             b2b_user_obj.trade_license_status = trade_license_status
             b2b_user_obj.passport_copy_status = passport_copy_status
+            b2b_user_obj.cohort = cohort
             b2b_user_obj.save()
 
             response["status"] = 200
