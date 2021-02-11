f62c2fff17e59512cca6c7fae5cb2b17bbebf657
mann27 <mannshah2712@gmail.com>
2021-02-11 16:30:49 +0530

    bug fixes

--- dealshub/constants.py
+++ dealshub/constants.py
@@ -1,4 +1,5 @@
 OMNYCOMM_IP = "http://13.232.143.29:8012"
 DEALSHUB_IP = "http://13.232.143.29:8020"
+SPOTII_AUTH_IP = "https://auth.sandbox.spotii.me"
 SPOTII_IP = "https://api.sandbox.spotii.me"
 WIGME_IP = "https://qa.wigme.com"
