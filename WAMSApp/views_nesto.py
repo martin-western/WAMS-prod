86813767397e8f2a75d4b0c2105b3d75cc3cff40
mann27 <mannshah2712@gmail.com>
2021-02-16 11:01:33 +0530

    added imports

--- WAMSApp/views_nesto.py
+++ WAMSApp/views_nesto.py
@@ -1,6 +1,14 @@
 from WAMSApp.models import *
 from rest_framework.views import APIView
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+import logging
+import json
+logger = logging.getLogger(__name__)
+
+
 class CreateNestoProductAPI(APIView):
 
     def post(self, request, *args, **kwargs):
