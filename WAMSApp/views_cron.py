5ac0e12fd5f5c5c32807af4614007eb925c1ca47
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2021-03-28 17:02:32 +0530

    bug fixes

--- WAMSApp/views_cron.py
+++ WAMSApp/views_cron.py
@@ -1,4 +1,4 @@
-from WAMSApp.models import *
+from dealshub.models import *
 from rest_framework.views import APIView
 from rest_framework import permissions, status
 from rest_framework.response import Response
