570e56d3866a73bbb90fab2f2a288073631b6aa0
nisargtike <nisarg.t.btechi14@ahduni.edu.in>
2021-03-28 15:37:51 +0530

    UnPublished Products CRON

--- WAMSApp/views.py
+++ WAMSApp/views.py
@@ -35,6 +35,7 @@ from WAMSApp.views_category_manager import *
 from WAMSApp.views_SAP_Integration import *
 from WAMSApp.utils_SAP_Integration import *
 from WAMSApp.views_nesto import *
+from WAMSApp.views_cron import *
 
 from PIL import Image as IMage
 from io import BytesIO as StringIO
