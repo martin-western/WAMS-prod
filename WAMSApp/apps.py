274d86392e3012fe68b726cd93caf541014ff5b3
davejay1999 <davejay1999@gmail.com>
2021-03-30 16:43:41 +0530

    WAMSApp apps undo change

--- WAMSApp/apps.py
+++ WAMSApp/apps.py
@@ -2,7 +2,4 @@ from django.apps import AppConfig
 
 
 class Stock_mangementConfig(AppConfig):
-    name = 'WAMSApp'
-
-    # def ready(self):
-    #     import signals 
+    name = 'Stock_mangement'
