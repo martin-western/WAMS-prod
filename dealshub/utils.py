09335dcb70c8882182231a6ef3537f2b9e25d11d
davejay1999 <davejay1999@gmail.com>
2021-05-04 16:16:20 +0530

    return status after delivery failed fixes

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -182,7 +182,6 @@ def set_order_status(unit_order_obj, order_status):
         unit_order_obj.current_status = "returned"
         unit_order_obj.save()
         UnitOrderStatus.objects.create(unit_order=unit_order_obj, status=status, status_admin=order_status)
-
         return
 
 
