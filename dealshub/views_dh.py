15da1ed92098a76faea5de462430c6888c7dc226
davejay1999 <davejay1999@gmail.com>
2021-05-20 11:59:28 +0530

    adding <dealshub/views_dh> extra response in fetchcartdetails

--- dealshub/views_dh.py
+++ dealshub/views_dh.py
@@ -712,8 +712,9 @@ class FetchCartDetailsAPI(APIView):
                 temp_dict["showNote"] = unit_cart_obj.product.is_promo_restriction_note_required(dealshub_user_obj)
                 temp_dict["moq"] = unit_cart_obj.product.get_moq(dealshub_user_obj)
                 temp_dict["stock"] = unit_cart_obj.product.stock
-                temp_dict["category"] = unit_cart_obj.product.category
-                temp_dict["sub_category"] = unit_cart_obj.product.sub_category
+                temp_dict["super_category"] = unit_cart_obj.product.get_super_category()        
+                temp_dict["category"] = unit_cart_obj.product.get_category()
+                temp_dict["sub_category"] = unit_cart_obj.product.get_sub_category()
                 temp_dict["allowedQty"] = unit_cart_obj.product.get_allowed_qty()
                 temp_dict["currency"] = unit_cart_obj.product.get_currency()
                 temp_dict["dateCreated"] = unit_cart_obj.get_date_created()
