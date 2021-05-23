fc8f6cac3b0467dd5b980f60569b16f17dddf938
Ubuntu <ubuntu@ip-172-31-37-55.ap-south-1.compute.internal>
2021-05-23 06:48:52 +0000

    SERVER COMMIT

--- dealshub/views_dh.py
+++ dealshub/views_dh.py
@@ -4225,7 +4225,7 @@ class SendB2BOTPSMSLoginAPI(APIView):
             for i in range(6):
                 OTP += digits[int(math.floor(random.random()*10))]
 
-            if contact_number in ["888888888", "940804016", "888888881", "702290032", "888888883"]:
+            if contact_number in ["888888888", "940804016", "888888881", "702290032", "888888883", "888888884"]:
                 OTP = "777777"
 
             otp_sent = False
