7e1cac6769703184cf7f3bb9f4922ec58ec3a297
Animesh Kumar <animesh-kumar@pop-os.localdomain>
2021-06-01 14:46:43 +0530

    fix (WAMSApp/views.py) fix SaveProductAPI

--- WAMSApp/views.py
+++ WAMSApp/views.py
@@ -1978,8 +1978,8 @@ class SaveProductAPI(APIView):
             dealshub_product_objs = custom_permission_filter_dealshub_product(request.user)
             dealshub_product_objs = dealshub_product_objs.filter(product=product_obj)
             for dealshub_product_obj in dealshub_product_objs:
-                dealshub_product_obj.product_name = product_name
-                dealshub_product_obj.product_description = product_description
+                dealshub_product_obj.product_name = data["product_name"]
+                dealshub_product_obj.product_description = data["product_description"]
                 dealshub_product_obj.save()
 
 
