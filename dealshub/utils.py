379efc5b1840a873850450c8044559ef1d267b7b
Animesh Kumar <24animesh11@gmail.com>
2021-06-02 15:46:02 +0530

    Update utils.py

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -100,7 +100,7 @@ def set_shipping_method(unit_order_obj, shipping_method):
 
 def set_order_status(unit_order_obj, order_status):
 
-    if unit_order_obj.current_status_admin == "picked" and order_status in ["approved"]:
+    if unit_order_obj.current_status_admin == "pending" and order_status in ["approved"]:
         if unit_order_obj.order.sap_manual_update_status:
             unit_order_obj.current_status_admin = "approved"
             unit_order_obj.save()
