2dd1aba0d598db21f38a77d4e8fb98156662af36
Animesh Kumar <animesh-kumar@pop-os.localdomain>
2021-06-08 18:58:44 +0530

    fix (WAMSApp/ | scripts/) Update SAP password for QA(uat)

--- scripts/update_wigme_stocks.py
+++ scripts/update_wigme_stocks.py
@@ -8,7 +8,7 @@ def fetch_refresh_stock(seller_sku, company_code, location_code):
     try:
         url="http://94.56.89.114:8001/sap/bc/srt/rfc/sap/zser_stock_price/300/zser_stock_price/zbin_stock_price"
         headers = {'content-type':'text/xml','accept':'application/json','cache-control':'no-cache'}
-        credentials = ("MOBSERVICE", "~lDT8+QklV=(")
+        credentials = (SAP_USERNAME, SAP_PASSWORD)
         body = """<soapenv:Envelope xmlns:urn="urn:sap-com:document:sap:rfc:functions" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
                   <soapenv:Header />
                   <soapenv:Body>
