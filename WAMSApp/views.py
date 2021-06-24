5ed03f30956a10191a850c5d25650248645527d5
rajmehta <rajmehta200012@gmail.com>
2021-06-24 15:55:06 +0530

    Added size-unit attribute

--- WAMSApp/views.py
+++ WAMSApp/views.py
@@ -860,7 +860,8 @@ class FetchProductDetailsAPI(APIView):
             response["color"] = product_obj.color
             response["weight"] = product_obj.weight
             response["dimensions"] = product_obj.get_dimensions()
-            response["size"] = "NA" if str(product_obj.size)=="" else str(product_obj.size + product_obj.size_unit)
+            response["size"] = "NA" if str(product_obj.size)=="" else str(product_obj.size) 
+            response["size_unit"] = str(product_obj.size_unit)
             response["capacity"] = "NA" if str(product_obj.capacity)=="" else str(product_obj.capacity)
             response["capacity_unit"] = str(product_obj.capacity_unit)
             
