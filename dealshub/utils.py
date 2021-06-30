8a71a3c82409340d9e35621f6da5652800dde043
davejay1999 <davejay1999@gmail.com>
2021-07-01 01:20:41 +0530

    adding logger

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -2271,7 +2271,7 @@ def calling_facebook_api(event_name,user,custom_data=None):
             events=events,
             pixel_id=pixel_id
         )
-        event_response = event_request.execute()
+        event_response = event_reqquest.execute()
         logger.info("calling_facebook_api success :- ",event_response)
 
     except Exception as e:
