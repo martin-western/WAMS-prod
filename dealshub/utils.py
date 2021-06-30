e97a46f4396a0467eb7e251940937d7984abf0b8
davejay1999 <davejay1999@gmail.com>
2021-07-01 01:15:26 +0530

    changing apikey

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -2230,7 +2230,7 @@ def calling_facebook_api(event_name,user,custom_data=None):
         country = sha256_encode(str(address_obj.get_country()))
         postcode = sha256_encode(str(address_obj.postcode))
 
-        access_token = "EAAFwjqw5ZBQoBAEe5EaMbMBROelIaf3BNX2GOdwNZCr3bve3JYPIhvE5AGKFtXrdjqDGcabrLcbgjxmI6FM5yZCCVvjGZAy8QEzUdxi4GprxP6tYApm03mTpawOUoZB5XqoNZB19JbNJZBdUEKdKgZBt6dG2HcvO07qefdikDjsVjZC2HAXsZAxsAZB"
+        access_token = "EAAFwjqw5ZBQoBAEPNNkat7AkfrDZCqDs9MIYf6jKHDdHTiqwrqwTZCHNBK4xHJqVZBoIvF1PBdl5dezVOZBE2NSzoXuwjM5Vq1ca5LvZC4D2UaJiZAZBXZAyWsWT7Y0sY7QKpSUsMppY3l91HuxLBHYFOorYcyZCDoJIi87mMtO6P9wmC9IzldfGTG"
         pixel_id = '501666847923989'
 
         FacebookAdsApi.init(access_token=access_token)
