2bfc76ad6e2d32551f46a6e9656942ad8b813868
davejay1999 <davejay1999@gmail.com>
2021-07-07 12:07:52 +0530

    logger in fb api

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -2233,7 +2233,7 @@ def visitor_ip_address(request):
     logger.info("ip visitor_ip_address :-    ", ip)
     return ip
 
-def calling_facebook_api(event_name,user,request,custom_data=None):
+def calling_facebook_api(event_name,user,request=None,custom_data=None):
     try:
         email = sha256_encode(str(user.email))
         first_name = sha256_encode(str(user.first_name))
