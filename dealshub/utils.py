ce4de692d5702ac8259223694c71bae01bc9aa48
davejay1999 <davejay1999@gmail.com>
2021-07-12 19:38:39 +0530

    adding response[meta]  fb api

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -2252,7 +2252,7 @@ def calling_facebook_api(event_name,user,request,custom_data=None):
             states=[state],
             zip_codes=[postcode],
             country_codes=[country],
-            client_ip_address="",
+            client_ip_address=request.META["HTTP_X_FORWARDED_FOR"],
             fbp= "fb.1.1625138246273.541394957",
         )
 
