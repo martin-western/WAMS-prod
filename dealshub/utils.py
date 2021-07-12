d4f707f08f6ab5c6691c1e588612f661a7b869a7
davejay1999 <davejay1999@gmail.com>
2021-07-12 20:24:18 +0530

    finding ip fb api

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -2233,9 +2233,7 @@ def calling_facebook_api(event_name,user,request,custom_data=None):
 
         access_token = "EAAFwjqw5ZBQoBAEPNNkat7AkfrDZCqDs9MIYf6jKHDdHTiqwrqwTZCHNBK4xHJqVZBoIvF1PBdl5dezVOZBE2NSzoXuwjM5Vq1ca5LvZC4D2UaJiZAZBXZAyWsWT7Y0sY7QKpSUsMppY3l91HuxLBHYFOorYcyZCDoJIi87mMtO6P9wmC9IzldfGTG"
         pixel_id = '501666847923989'
-        event_source_url = "https://qa.wigme.com"
-        #prod
-        # event_source_url = "https://www.wigme.com"
+        event_source_url = "https://www.wigme.com"
 
         now_time = int(time.time())
         logger.info("in calling_facebook_api:- ")
