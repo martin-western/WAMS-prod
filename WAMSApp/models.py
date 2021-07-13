6273ddd6ca84aa3bbb630116244cd7b892276d7d
davejay1999 <davejay1999@gmail.com>
2021-07-13 12:18:34 +0530

    adding logger in class image

--- WAMSApp/models.py
+++ WAMSApp/models.py
@@ -284,7 +284,7 @@ class Image(models.Model):
                     thumb.save(thumb_io, im_type)
                     thumb_file = InMemoryUploadedFile(thumb_io, None, infile, 'image/'+im_type, thumb_io.getbuffer().nbytes, None)
                     self.optimal_image = thumb_file
-                    logger.info(self.optimal_image)            
+                    logger.info(self.optimal_image.url)            
                         
             if self.thumbnail == None:
                 size = 128, 128
