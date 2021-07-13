231e91d7b5574742de87a4ceccd426df28fb5ba5
davejay1999 <davejay1999@gmail.com>
2021-07-13 14:34:52 +0530

    change need for prod in fb api

--- dealshub/utils.py
+++ dealshub/utils.py
@@ -2260,7 +2260,7 @@ def calling_facebook_api(event_name,user,request,custom_data=None):
 
         access_token = "EAAFwjqw5ZBQoBAEPNNkat7AkfrDZCqDs9MIYf6jKHDdHTiqwrqwTZCHNBK4xHJqVZBoIvF1PBdl5dezVOZBE2NSzoXuwjM5Vq1ca5LvZC4D2UaJiZAZBXZAyWsWT7Y0sY7QKpSUsMppY3l91HuxLBHYFOorYcyZCDoJIi87mMtO6P9wmC9IzldfGTG"
         pixel_id = '501666847923989'
-        event_source_url = "https://qa.wigme.com"
+        event_source_url = "https://www.wigme.com"
 
         now_time = int(time.time())
         logger.info("in calling_facebook_api:- ")
@@ -2309,7 +2309,7 @@ def calling_facebook_api(event_name,user,request,custom_data=None):
         event_request = EventRequest(
             events=events,
             pixel_id=pixel_id,
-            # test_event_code="TEST63889",
+            test_event_code="TEST63889",
         )
         event_response = event_request.execute()
         logger.info(event_response)
