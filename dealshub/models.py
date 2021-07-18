c45661bdd48bb5a21d169d45061af9d083a59a7f
davejay1999 <davejay1999@gmail.com>
2021-07-18 12:47:17 +0530

    adding network global logger <dealshub.models,network_global_integration.p.py>

--- dealshub/models.py
+++ dealshub/models.py
@@ -1991,7 +1991,7 @@ class NetworkGlobalLog(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     request = models.TextField(blank = True,default='{}')
     response = models.TextField(blank = True,default='{}')
-    status = models.TextField(blank = True,default="")
+    status_code = models.TextField(blank = True,default="")
     merchant_reference=models.CharField(max_length=256, blank=True, default='')
 
     def __str__(self):
