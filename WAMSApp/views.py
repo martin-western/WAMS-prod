9378d3419d6622c9cdd80c7b0147650a106aefe0
davejay1999 <davejay1999@gmail.com>
2021-09-20 17:36:32 +0530

    adding run()

--- WAMSApp/views.py
+++ WAMSApp/views.py
@@ -7062,7 +7062,7 @@ class CreateOCReportAPI(APIView):
                 logger.warning("CreateOCReportAPI9")
                 # p1 = multiprocessing.Process(target=bulk_download_nesto_detailed_product_report, args=(filename,oc_report_obj.uuid,))
                 p1 = threading.Thread(target=bulk_download_nesto_detailed_product_report, args=(filename,oc_report_obj.uuid,),daemon = True)
-                p1.start()
+                p1.run()
                 logger.warning("CreateOCReportAPI10")
             elif report_type.lower()=="nesto product summary":
                 p1 = threading.Thread(target=nesto_products_summary_report, args=(filename,oc_report_obj.uuid,))
