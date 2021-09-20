ad327832357f6d5aafabb2273de9fc5673b5ebc3
davejay1999 <davejay1999@gmail.com>
2021-09-20 17:32:51 +0530

    debuging nesto reports

--- WAMSApp/views.py
+++ WAMSApp/views.py
@@ -7061,7 +7061,7 @@ class CreateOCReportAPI(APIView):
             elif report_type.lower()=="nesto detailed product":
                 logger.warning("CreateOCReportAPI9")
                 # p1 = multiprocessing.Process(target=bulk_download_nesto_detailed_product_report, args=(filename,oc_report_obj.uuid,))
-                p1 = threading.Thread(target=bulk_download_nesto_detailed_product_report, args=(filename,oc_report_obj.uuid,))
+                p1 = threading.Thread(target=bulk_download_nesto_detailed_product_report, args=(filename,oc_report_obj.uuid,),daemon = True)
                 p1.start()
                 logger.warning("CreateOCReportAPI10")
             elif report_type.lower()=="nesto product summary":
