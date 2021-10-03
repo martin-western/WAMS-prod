cda37bacedbc0ffea0066e93f6f317e7a7eee1d4
davejay1999 <davejay1999@gmail.com>
2021-10-03 14:36:36 +0530

    addind nestob2b super cat

--- dealshub/views.py
+++ dealshub/views.py
@@ -742,33 +742,44 @@ class FetchSuperCategoriesAPI(APIView):
 
             super_category_list = []
             for super_category_obj in super_category_objs:
-                temp_dict = {}
-                temp_dict["name"] = super_category_obj.get_name(language_code)
-                temp_dict["name_en"] = super_category_obj.get_name("en")
-                temp_dict["uuid"] = super_category_obj.uuid
-                temp_dict["imageUrl"] = ""
-                if super_category_obj.image!=None:
-                    temp_dict["imageUrl"] = super_category_obj.image.thumbnail.url
-                
-                category_list = []
-                category_objs = Category.objects.filter(super_category=super_category_obj)[:30]
-                for category_obj in category_objs:
-                    temp_dict2 = {}
-                    temp_dict2["category_name"] = category_obj.get_name(language_code)
-                    temp_dict2["category_name_en"] = category_obj.get_name("en")
+                try:
+                    temp_dict = {}
+                    temp_dict["name"] = super_category_obj.get_name(language_code)
+                    temp_dict["name_en"] = super_category_obj.get_name("en")
+                    temp_dict["uuid"] = super_category_obj.uuid
+                    temp_dict["imageUrl"] = ""
+                    if super_category_obj.image!=None:
+                        temp_dict["imageUrl"] = super_category_obj.image.thumbnail.url
                     
-                    sub_category_list = []
-                    sub_category_objs = SubCategory.objects.filter(category=category_obj)[:30]
-                    for sub_category_obj in sub_category_objs:
-                        temp_dict3 = {}
-                        temp_dict3["sub_category_name"] = sub_category_obj.get_name(language_code)
-                        temp_dict3["sub_category_name_en"] = sub_category_obj.get_name("en")
-                        if DealsHubProduct.objects.filter(is_published=True, sub_category=sub_category_obj, location_group__website_group=website_group_obj, product__base_product__brand__in=website_group_obj.brands.all()).exclude(now_price=0).exclude(stock=0).exists():
-                            sub_category_list.append(temp_dict3)
-                    
-                    temp_dict2["sub_category_list"] = sub_category_list
-                    if DealsHubProduct.objects.filter(is_published=True, category=category_obj, location_group__website_group=website_group_obj, product__base_product__brand__in=website_group_obj.brands.all()).exclude(now_price=0).exclude(stock=0).exists():
-                        category_list.append(temp_dict2)
+                    category_list = []
+                    category_objs = Category.objects.filter(super_category=super_category_obj)[:30]
+                    for category_obj in category_objs:
+                        try:
+                            temp_dict2 = {}
+                            temp_dict2["category_name"] = category_obj.get_name(language_code)
+                            temp_dict2["category_name_en"] = category_obj.get_name("en")
+                            
+                            sub_category_list = []
+                            sub_category_objs = SubCategory.objects.filter(category=category_obj)[:30]
+                            for sub_category_obj in sub_category_objs:
+                                try:
+                                    temp_dict3 = {}
+                                    temp_dict3["sub_category_name"] = sub_category_obj.get_name(language_code)
+                                    temp_dict3["sub_category_name_en"] = sub_category_obj.get_name("en")
+                                    if DealsHubProduct.objects.filter(is_published=True, sub_category=sub_category_obj, location_group__website_group=website_group_obj, product__base_product__brand__in=website_group_obj.brands.all()).exclude(now_price=0).exclude(stock=0).exists():
+                                        sub_category_list.append(temp_dict3)
+                                except Exception as e:
+                                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                                    logger.error("FetchSuperCategoriesAPI: %s at %s", e, str(exc_tb.tb_lineno))
+                            temp_dict2["sub_category_list"] = sub_category_list
+                            if DealsHubProduct.objects.filter(is_published=True, category=category_obj, location_group__website_group=website_group_obj, product__base_product__brand__in=website_group_obj.brands.all()).exclude(now_price=0).exclude(stock=0).exists():
+                                category_list.append(temp_dict2)
+                        except Exception as e:
+                            exc_type, exc_obj, exc_tb = sys.exc_info()
+                            logger.error("FetchSuperCategoriesAPI: %s at %s", e, str(exc_tb.tb_lineno))
+                except Exception as e:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    logger.error("FetchSuperCategoriesAPI: %s at %s", e, str(exc_tb.tb_lineno))                
 
                 temp_dict["category_list"] = category_list
                 if len(category_list)>0:
@@ -789,7 +800,7 @@ class FetchSuperCategoriesAPI(APIView):
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             logger.error("FetchSuperCategoriesAPI: %s at %s", e, str(exc_tb.tb_lineno))
-        
+        logger.info(response)
         return Response(data=response)
 
 
