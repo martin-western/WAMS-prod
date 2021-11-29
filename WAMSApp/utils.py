da7c5cba4a8fa7010087f1caccafcd79d6a3e6c8
davejay1999 <davejay1999@gmail.com>
2021-11-29 18:11:59 +0530

    extra whitespace

--- WAMSApp/utils.py
+++ WAMSApp/utils.py
@@ -2974,7 +2974,7 @@ def create_all_dealshub_products_report(filename, uuid, location_group_obj):
     oc_report_obj.is_processed = True
     oc_report_obj.completion_date = timezone.now()
     oc_report_obj.save()
-    
+
     notify_user_for_report(oc_report_obj)
 
 def activitylog(user,table_name,action_type,table_item_pk='',prev_instance=None,current_instance=None,location_group_obj=None,render='', is_nesto=False):
