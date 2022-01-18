1e8495167af72c6dae3f8ffd44daf9fcd2060efb
rajmehta <rajmehta200012@gmail.com>
2022-01-18 09:56:53 +0530

    Bug Fixing in Net sales of Analytics

--- dealshub/views_dh.py
+++ dealshub/views_dh.py
@@ -7062,8 +7062,8 @@ class FetchSalesExecutiveAnalysisAPI(APIView):
                     "monthly_returned_amount" : total_monthly_orders_status_amount_list[3],
                     "cancelled": total_monthly_orders_status_count_list[4],
                     "monthly_cancelled_amount" : total_monthly_orders_status_amount_list[4],
-                    "net_sales" : month_total_orders - total_monthly_orders_status_count_list[3],
-                    "net_sales_amount" : round(float(month_total_sales - total_monthly_orders_status_amount_list[3]),2)
+                    "net_sales" : month_total_orders - total_monthly_orders_status_count_list[3] - total_monthly_orders_status_count_list[4],
+                    "net_sales_amount" : round(float(month_total_sales - total_monthly_orders_status_amount_list[3] - total_monthly_orders_status_amount_list[4]),2)
                 }
                 temp_dict["currency"] = location_group_obj.location.currency
                 temp_dict["username"] = sales_target_obj.user.username
@@ -7212,8 +7212,8 @@ class FetchOrderSalesAnalyticsAPI(APIView):
                 "today_returned_amount" : total_orders_status_amount_list[3],
                 "cancelled": total_orders_status_count_list[4],
                 "today_cancelled_amount" : total_orders_status_amount_list[4],
-                "net_sales" : today_total_orders - total_orders_status_count_list[3],
-                "net_sales_amount" : round(float(today_total_sales - total_orders_status_amount_list[3]),2)
+                "net_sales" : today_total_orders - total_orders_status_count_list[3] - total_orders_status_count_list[4],
+                "net_sales_amount" : round(float(today_total_sales - total_orders_status_amount_list[3] - total_orders_status_amount_list[4]),2)
 
             }
             response["monthly"] = {
@@ -7230,8 +7230,8 @@ class FetchOrderSalesAnalyticsAPI(APIView):
                 "monthly_returned_amount" : total_monthly_orders_status_amount_list[3],
                 "cancelled": total_monthly_orders_status_count_list[4],
                 "monthly_cancelled_amount" : total_monthly_orders_status_amount_list[4],
-                "net_sales" : month_total_orders - total_monthly_orders_status_count_list[3],
-                "net_sales_amount" : round(float(month_total_sales - total_monthly_orders_status_amount_list[3]),2)
+                "net_sales" : month_total_orders - total_monthly_orders_status_count_list[3] - total_monthly_orders_status_count_list[4],
+                "net_sales_amount" : round(float(month_total_sales - total_monthly_orders_status_amount_list[3] - total_monthly_orders_status_amount_list[4]),2)
  
             }
             response["currency"] = location_group_obj.location.currency
@@ -7377,8 +7377,8 @@ class FetchFilteredOrderAnalyticsAPI(APIView):
                 "filtered_returned_amount" : total_filtered_orders_status_amount_list[3],
                 "cancelled": total_filtered_orders_status_count_list[4],
                 "filtered_cancelled_amount" : total_filtered_orders_status_amount_list[4],
-                "net_sales" : real_total_orders - total_filtered_orders_status_count_list[3], 
-                "net_sales_amount" : round(float(total_sales - total_filtered_orders_status_amount_list[3]),2)
+                "net_sales" : real_total_orders - total_filtered_orders_status_count_list[3] - total_filtered_orders_status_count_list[4], 
+                "net_sales_amount" : round(float(total_sales - total_filtered_orders_status_amount_list[3] - total_filtered_orders_status_amount_list[4]),2)
             }
             response["status"] = 200
 
